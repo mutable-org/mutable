18cf6a4e3ffeb0a8795a4d67e6853c6d7e7e02de
Immanuel Haffner <haffner.immanuel@gmail.com>
2019-10-15 11:44:59 +0200

    Modify the dummy join ordering algorithm to produce plans that are in correct RPO notation.  Performs a depth first search to find a valid plan.

--- src/IR/JoinOrderer.cpp
+++ src/IR/JoinOrderer.cpp
@@ -16,19 +16,37 @@ DummyJoinOrderer::mapping_type DummyJoinOrderer::operator()(const JoinGraph &G, 
     while (not worklist.empty()) {
         auto G = worklist.back();
         worklist.pop_back();
-
         auto &order = map[G];
 
-        std::unordered_set<const DataSource*> joined; ///< set of already joined data sources
+        std::unordered_set<const DataSource*> frontier;
+        frontier.insert(*G->sources().begin());
+        std::unordered_set<const Join*> joins(G->joins().begin(), G->joins().end()); ///< set of available joins
+        std::unordered_set<const DataSource*> used; ///< set of already joined data sources
 
-        for (auto J : G->joins()) {
-            for (auto S : J->sources()) {
-                if (not joined.count(S)) {
-                    order.push_back(S);
-                    joined.insert(S);
+        while (not frontier.empty()) {
+            /* Pick the next relation from the frontier. */
+            auto r = *frontier.begin();
+            used.insert(r);
+            order.push_back(r); // emit
+
+            /* Apply all possible joins. */
+            std::vector<const Join*> joins_used;
+            for (auto j : joins) { // TODO apply smaller joins first to get better plans
+                if (subset(j->sources(), used)) {
+                    joins_used.push_back(j);
+                    order.push_back(j); // emit
                 }
             }
-            order.push_back(J);
+            for (auto j : joins_used) joins.erase(j); // erase the used joins so every join is used exactly once
+
+            /* Update frontier.  Explore 'r' by adding all its unexplored neighbors to the frontier. */
+            frontier.erase(r);
+            for (auto j : r->joins()) {
+                for (auto n : j->sources()) {
+                    if (used.count(n) == 0)
+                        frontier.insert(n);
+                }
+            }
         }
     }
 

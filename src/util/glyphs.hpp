ce85c4234f4ff5614e460f0d4bfac159da001791
Immanuel Haffner <haffner.immanuel@gmail.com>
2019-10-24 11:57:13 +0200

    Introduce a file that collects interesting glyphs so they can easily be found and used.

---
+++ src/util/glyphs.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+
+namespace glyphs {
+
+constexpr auto CHEVRON_RIGHT    = u8"\uf054"; // 
+constexpr auto TRIANGLE_RIGHT   = u8"\uf44a"; // 
+constexpr auto DATABASE         = u8"\uf6b7"; // 
+constexpr auto LIGHTNING        = u8"\ue00a"; // 
+constexpr auto ELECTRON         = u8"\ue62e"; // 
+constexpr auto RIGHT            = u8"";      // 
+//  ﱦ  
+
+}

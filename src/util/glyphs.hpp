ddd02ddf23f28c784c4d99e0cc4f6edd55e3b549
Immanuel Haffner <haffner.immanuel@gmail.com>
2019-10-25 09:42:33 +0200

    Add a clock glyph.

--- src/util/glyphs.hpp
+++ src/util/glyphs.hpp
@@ -10,5 +10,6 @@ constexpr auto LIGHTNING        = u8"\ue00a"; // 
 constexpr auto ELECTRON         = u8"\ue62e"; // 
 constexpr auto RIGHT            = u8"";      // 
 //  ﱦ  
+constexpr auto CLOCK_FAST       = u8"\uf651"; // 
 
 }

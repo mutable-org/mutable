9d0af8c14c7685d3552250b2b87a18735b164820
Immanuel Haffner <haffner.immanuel@gmail.com>
2019-11-08 12:16:20 +0100

    Add end2end tests for the `DSVReader`.  In particular, test whether the `DSVReader` can handle the optional header and permuted or superfluous columns.

---
+++ test/end2end/positive/queries/read_dsv_has_header_permuted.sql
@@ -0,0 +1,9 @@
+CREATE TABLE test (
+    a INT(4),
+    b INT(4),
+    c INT(4)
+);
+
+IMPORT INTO test DSV "test/end2end/positive/data/read_dsv_has_header_permuted.csv" HAS HEADER;
+
+SELECT * FROM test;

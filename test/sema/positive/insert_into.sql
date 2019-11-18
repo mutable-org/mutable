63ee09d837172e03aeb001cb6fa2653a8af5fb6f
Immanuel Haffner <haffner.immanuel@gmail.com>
2019-11-18 09:48:27 +0100

    Add sema test case for `INSERT INTO` statements.

---
+++ test/sema/positive/insert_into.sql
@@ -0,0 +1,7 @@
+INSERT INTO NATION
+VALUES
+    (0, "Germany", 42, "Einigkeit und Recht und Freiheit"),
+    (1, "United States", 1, "United States of America"),
+    (2, NULL, NULL, NULL),
+    (DEFAULT, "Iceland", 3, NULL)
+;

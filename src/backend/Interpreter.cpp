mutable-org/mutable

commit 02b25a610123eacad3e452711865cefff7e04f1f

Split the `Interpreter` into two parts.  One part finds the start of the pipelines and prepares the operators for evaluation.  The other part, named `Pipeline`, pushes tuples through the pipeline to the next pipeline breaker or final consumer.

src/backend/Interpreter.hpp
@@ -10,6 +10,39 @@
 
 namespace db {
 
+/** Implements push-based evaluation of a pipeline in the plan. */
+struct Pipeline : ConstOperatorVisitor
+{
+    private:
+    tuple_type tuple_;
+
+    Pipeline() = default;
+    Pipeline(tuple_type t) : tuple_(t) { }
+
+    public:
+    static void Push(const Operator &pipeline_start) {
+        Pipeline P;
+        P(pipeline_start);
+    }
+
+    static void Push(const Operator &pipeline_start, tuple_type t) {
+        Pipeline P(t);
+        P(pipeline_start);
+    }
+
+    using ConstOperatorVisitor::operator();
+#define DECLARE(CLASS) void operator()(Const<CLASS> &op) override
+    DECLARE(ScanOperator);
+    DECLARE(CallbackOperator);
+    DECLARE(FilterOperator);
+    DECLARE(JoinOperator);
+    DECLARE(ProjectionOperator);
+    DECLARE(LimitOperator);
+    DECLARE(GroupingOperator);
+    DECLARE(SortingOperator);
+#undef DECLARE
+};
+
 /** Evaluates SQL operator trees on the database. */
 struct Interpreter : Backend, ConstOperatorVisitor
 {
@@ -19,24 +52,15 @@ struct Interpreter : Backend, ConstOperatorVisitor
     void execute(const Operator &plan) const override { (*const_cast<Interpreter*>(this))(plan); }
 
     using ConstOperatorVisitor::operator();
-
-#define DECLARE(CLASS) \
-    void operator()(Const<CLASS> &op) override
-#define DECLARE_CONSUMER(CLASS) \
-    DECLARE(CLASS); \
-    void operator()(Const<CLASS> &op, tuple_type &t) override
-
+#define DECLARE(CLASS) void operator()(Const<CLASS> &op) override
     DECLARE(ScanOperator);
-
-    DECLARE_CONSUMER(CallbackOperator);
-    DECLARE_CONSUMER(FilterOperator);
-    DECLARE_CONSUMER(JoinOperator);
-    DECLARE_CONSUMER(ProjectionOperator);
-    DECLARE_CONSUMER(LimitOperator);
-    DECLARE_CONSUMER(GroupingOperator);
-    DECLARE_CONSUMER(SortingOperator);
-
-#undef DECLARE_CONSUMER
+    DECLARE(CallbackOperator);
+    DECLARE(FilterOperator);
+    DECLARE(JoinOperator);
+    DECLARE(ProjectionOperator);
+    DECLARE(LimitOperator);
+    DECLARE(GroupingOperator);
+    DECLARE(SortingOperator);
 #undef DECLARE
 
     static value_type eval(const Constant &c)

src/IR/Operator.cpp
@@ -263,22 +263,15 @@ struct SchemaMinimizer : OperatorVisitor
     public:
     using OperatorVisitor::operator();
 
-#define DECLARE(CLASS) \
-    void operator()(Const<CLASS> &op) override
-#define DECLARE_CONSUMER(CLASS) \
-    void operator()(Const<CLASS>&, tuple_type&) override { } \
-    DECLARE(CLASS)
-
+#define DECLARE(CLASS) void operator()(Const<CLASS> &op) override
     DECLARE(ScanOperator);
-    DECLARE_CONSUMER(CallbackOperator);
-    DECLARE_CONSUMER(FilterOperator);
-    DECLARE_CONSUMER(JoinOperator);
-    DECLARE_CONSUMER(ProjectionOperator);
-    DECLARE_CONSUMER(LimitOperator);
-    DECLARE_CONSUMER(GroupingOperator);
-    DECLARE_CONSUMER(SortingOperator);
-
-#undef DECLARE_CONSUMER
+    DECLARE(CallbackOperator);
+    DECLARE(FilterOperator);
+    DECLARE(JoinOperator);
+    DECLARE(ProjectionOperator);
+    DECLARE(LimitOperator);
+    DECLARE(GroupingOperator);
+    DECLARE(SortingOperator);
 #undef DECLARE
 };
 

src/IR/Operator.hpp
@@ -278,10 +278,6 @@ struct Consumer : virtual Operator
     const std::vector<Producer*> & children() const { return children_; }
     Producer * child(std::size_t i) const { insist(i < children_.size()); return children_[i]; }
 
-    using Operator::accept;
-    virtual void accept(OperatorVisitor &V, tuple_type &T) = 0;
-    virtual void accept(ConstOperatorVisitor &V, tuple_type &T) const = 0;
-
     void print_recursive(std::ostream &out, unsigned depth) const override;
 
     protected:
@@ -301,8 +297,6 @@ struct CallbackOperator : Consumer
 
     void accept(OperatorVisitor &v) override;
     void accept(ConstOperatorVisitor &v) const override;
-    void accept(OperatorVisitor &V, tuple_type &T) override;
-    void accept(ConstOperatorVisitor &V, tuple_type &T) const override;
 
     private:
     void print(std::ostream &out) const override;
@@ -345,8 +339,6 @@ struct FilterOperator : Producer, Consumer
 
     void accept(OperatorVisitor &v) override;
     void accept(ConstOperatorVisitor &v) const override;
-    void accept(OperatorVisitor &v, tuple_type &t) override;
-    void accept(ConstOperatorVisitor &v, tuple_type &t) const override;
 
     private:
     void print(std::ostream &out) const override;
@@ -377,8 +369,6 @@ struct JoinOperator : Producer, Consumer
 
     void accept(OperatorVisitor &v) override;
     void accept(ConstOperatorVisitor &v) const override;
-    void accept(OperatorVisitor &v, tuple_type &t) override;
-    void accept(ConstOperatorVisitor &v, tuple_type &t) const override;
 
     private:
     void print(std::ostream &out) const override;
@@ -430,8 +420,6 @@ struct ProjectionOperator : Producer, Consumer
 
     void accept(OperatorVisitor &v) override;
     void accept(ConstOperatorVisitor &v) const override;
-    void accept(OperatorVisitor &v, tuple_type &t) override;
-    void accept(ConstOperatorVisitor &v, tuple_type &t) const override;
 
     private:
     void print(std::ostream &out) const override;
@@ -457,8 +445,6 @@ struct LimitOperator : Producer, Consumer
 
     void accept(OperatorVisitor &v) override;
     void accept(ConstOperatorVisitor &v) const override;
-    void accept(OperatorVisitor &v, tuple_type &t) override;
-    void accept(ConstOperatorVisitor &v, tuple_type &t) const override;
 
     private:
     void print(std::ostream &out) const override;
@@ -506,8 +492,6 @@ struct GroupingOperator : Producer, Consumer
 
     void accept(OperatorVisitor &v) override;
     void accept(ConstOperatorVisitor &v) const override;
-    void accept(OperatorVisitor &v, tuple_type &t) override;
-    void accept(ConstOperatorVisitor &v, tuple_type &t) const override;
 
     private:
     void print(std::ostream &out) const override;
@@ -529,8 +513,6 @@ struct SortingOperator : Producer, Consumer
 
     void accept(OperatorVisitor &v) override;
     void accept(ConstOperatorVisitor &v) const override;
-    void accept(OperatorVisitor &v, tuple_type &t) override;
-    void accept(ConstOperatorVisitor &v, tuple_type &t) const override;
 
     private:
     void print(std::ostream &out) const override;

src/IR/OperatorDot.hpp
@@ -17,23 +17,15 @@ struct OperatorDot : ConstOperatorVisitor
     ~OperatorDot();
 
     using ConstOperatorVisitor::operator();
-
-#define DECLARE(CLASS) \
-    void operator()(Const<CLASS> &op) override
-#define DECLARE_CONSUMER(CLASS) \
-    void operator()(Const<CLASS>&, tuple_type&) override { } \
-    DECLARE(CLASS)
-
+#define DECLARE(CLASS) void operator()(Const<CLASS> &op) override
     DECLARE(ScanOperator);
-    DECLARE_CONSUMER(CallbackOperator);
-    DECLARE_CONSUMER(FilterOperator);
-    DECLARE_CONSUMER(JoinOperator);
-    DECLARE_CONSUMER(ProjectionOperator);
-    DECLARE_CONSUMER(LimitOperator);
-    DECLARE_CONSUMER(GroupingOperator);
-    DECLARE_CONSUMER(SortingOperator);
-
-#undef DECLARE_CONSUMER
+    DECLARE(CallbackOperator);
+    DECLARE(FilterOperator);
+    DECLARE(JoinOperator);
+    DECLARE(ProjectionOperator);
+    DECLARE(LimitOperator);
+    DECLARE(GroupingOperator);
+    DECLARE(SortingOperator);
 #undef DECLARE
 };
 

src/IR/OperatorVisitor.cpp
@@ -12,17 +12,11 @@ using namespace db;
     void CLASS::accept(OperatorVisitor &v)            { v(*this); } \
     void CLASS::accept(ConstOperatorVisitor &v) const { v(*this); }
 
-#define ACCEPT_CONSUMER(CLASS) \
-    ACCEPT(CLASS) \
-    void CLASS::accept(OperatorVisitor &v, tuple_type &t)            { v(*this, t); } \
-    void CLASS::accept(ConstOperatorVisitor &v, tuple_type &t) const { v(*this, t); }
-
 ACCEPT(ScanOperator);
-
-ACCEPT_CONSUMER(CallbackOperator);
-ACCEPT_CONSUMER(FilterOperator);
-ACCEPT_CONSUMER(JoinOperator);
-ACCEPT_CONSUMER(ProjectionOperator);
-ACCEPT_CONSUMER(LimitOperator);
-ACCEPT_CONSUMER(GroupingOperator);
-ACCEPT_CONSUMER(SortingOperator);
+ACCEPT(CallbackOperator);
+ACCEPT(FilterOperator);
+ACCEPT(JoinOperator);
+ACCEPT(ProjectionOperator);
+ACCEPT(LimitOperator);
+ACCEPT(GroupingOperator);
+ACCEPT(SortingOperator);

src/IR/OperatorVisitor.hpp
@@ -16,24 +16,15 @@ struct TheOperatorVisitor
     virtual ~TheOperatorVisitor() { }
 
     void operator()(Const<Operator> &op) { op.accept(*this); }
-    void operator()(Const<Operator> &op, tuple_type &t) { op.accept(*this, t); }
-
-#define DECLARE(CLASS) \
-    virtual void operator()(Const<CLASS> &op) = 0
-#define DECLARE_CONSUMER(CLASS) \
-    DECLARE(CLASS); \
-    virtual void operator()(Const<CLASS> &op, tuple_type &t) = 0
-
+#define DECLARE(CLASS) virtual void operator()(Const<CLASS> &op) = 0
     DECLARE(ScanOperator);
-    DECLARE_CONSUMER(CallbackOperator);
-    DECLARE_CONSUMER(FilterOperator);
-    DECLARE_CONSUMER(JoinOperator);
-    DECLARE_CONSUMER(ProjectionOperator);
-    DECLARE_CONSUMER(LimitOperator);
-    DECLARE_CONSUMER(GroupingOperator);
-    DECLARE_CONSUMER(SortingOperator);
-
-#undef DECLARE_CONSUMER
+    DECLARE(CallbackOperator);
+    DECLARE(FilterOperator);
+    DECLARE(JoinOperator);
+    DECLARE(ProjectionOperator);
+    DECLARE(LimitOperator);
+    DECLARE(GroupingOperator);
+    DECLARE(SortingOperator);
 #undef DECLARE
 };
 

src/backend/WebAssembly.cpp
@@ -81,12 +81,14 @@ WASMModule WASMCodeGen::compile(const Operator &op)
     return module;
 }
 
+
 /*======================================================================================================================
- * WASMCodeGen / Recursive descent
+ * WASMCodeGen / Operators
  *====================================================================================================================*/
 
 void WASMCodeGen::operator()(const ScanOperator &op)
 {
+    // TODO implement
 }
 
 void WASMCodeGen::operator()(const CallbackOperator &op)
@@ -96,10 +98,12 @@ void WASMCodeGen::operator()(const CallbackOperator &op)
 
 void WASMCodeGen::operator()(const FilterOperator &op)
 {
+    // TODO implement
 }
 
 void WASMCodeGen::operator()(const JoinOperator &op)
 {
+    // TODO implement
 }
 
 void WASMCodeGen::operator()(const ProjectionOperator &op)
@@ -121,47 +125,19 @@ void WASMCodeGen::operator()(const ProjectionOperator &op)
 
 void WASMCodeGen::operator()(const LimitOperator &op)
 {
+    // TODO implement
 }
 
 void WASMCodeGen::operator()(const GroupingOperator &op)
 {
+    // TODO implement
 }
 
 void WASMCodeGen::operator()(const SortingOperator &op)
 {
+    // TODO implement
 }
 
-/*======================================================================================================================
- * WASMCodeGen / Recursive ascent
- *====================================================================================================================*/
-
-void WASMCodeGen::operator()(const FilterOperator &op, tuple_type &t)
-{
-}
-
-void WASMCodeGen::operator()(const CallbackOperator &op, tuple_type &t)
-{
-}
-
-void WASMCodeGen::operator()(const JoinOperator &op, tuple_type &t)
-{
-}
-
-void WASMCodeGen::operator()(const ProjectionOperator &op, tuple_type &t)
-{
-}
-
-void WASMCodeGen::operator()(const LimitOperator &op, tuple_type &t)
-{
-}
-
-void WASMCodeGen::operator()(const GroupingOperator &op, tuple_type &t)
-{
-}
-
-void WASMCodeGen::operator()(const SortingOperator &op, tuple_type &t)
-{
-}
 
 /*======================================================================================================================
  * WASMCodeGen / Expressions
@@ -169,6 +145,7 @@ void WASMCodeGen::operator()(const SortingOperator &op, tuple_type &t)
 
 void WASMCodeGen::operator()(Const<Designator> &e)
 {
+    // TODO implement
 }
 
 void WASMCodeGen::operator()(Const<Constant> &e)
@@ -213,6 +190,8 @@ void WASMCodeGen::operator()(Const<Constant> &e)
 
 void WASMCodeGen::operator()(Const<FnApplicationExpr> &e)
 {
+    (void) e;
+    unreachable("not implemented");
 }
 
 void WASMCodeGen::operator()(Const<UnaryExpr> &e)
@@ -416,6 +395,11 @@ void WASMCodeGen::operator()(Const<BinaryExpr> &e)
 #undef BINARY
 #undef BINARY_OP
 }
+
+
+/*======================================================================================================================
+ * WASMBackend
+ *====================================================================================================================*/
 
 void WASMBackend::execute(const Operator &plan) const
 {

commit 8d508a71762e167ad619f2b7278ba288d24c434a

Fix a performance regression introduced by 02b25a6.  Pass the results produced by the stack machine by reference instead of by value.  This avoids redundant copying.  Especially scans benefit from this optimization.

src/backend/Interpreter.cpp
@@ -134,6 +134,8 @@ struct FilterData : OperatorData
  * Pipeline
  *====================================================================================================================*/
 
+tuple_type Pipeline::empty;
+
 void Pipeline::operator()(const ScanOperator &op)
 {
     auto loader = op.store().loader(op.schema());
@@ -475,8 +477,10 @@ void Interpreter::operator()(const GroupingOperator &op)
             for (auto e : op.group_by())
                 data->keys.emit(*e);
             op.child(0)->accept(*this);
-            for (auto g : data->groups)
-                Pipeline::Push(parent, g.first + g.second); // pass groups on to parent
+            for (auto g : data->groups) {
+                auto t = g.first + g.second;
+                Pipeline::Push(parent, t); // pass groups on to parent
+            }
             break;
         }
     }

src/backend/Interpreter.hpp
@@ -14,10 +14,11 @@ namespace db {
 struct Pipeline : ConstOperatorVisitor
 {
     private:
-    tuple_type tuple_;
+    static tuple_type empty;
+    tuple_type &tuple_;
 
-    Pipeline() = default;
-    Pipeline(tuple_type t) : tuple_(t) { }
+    Pipeline() : tuple_(empty) { }
+    Pipeline(tuple_type &t) : tuple_(t) { }
 
     public:
     static void Push(const Operator &pipeline_start) {
@@ -25,7 +26,7 @@ struct Pipeline : ConstOperatorVisitor
         P(pipeline_start);
     }
 
-    static void Push(const Operator &pipeline_start, tuple_type t) {
+    static void Push(const Operator &pipeline_start, tuple_type &t) {
         Pipeline P(t);
         P(pipeline_start);
     }

src/backend/StackMachine.cpp
@@ -547,7 +547,7 @@ void StackMachine::emit(const cnf::CNF &cnf)
     }
 }
 
-tuple_type StackMachine::operator()(const tuple_type &t)
+const tuple_type & StackMachine::operator()(const tuple_type &t)
 {
     static const void *labels[] = {
 #define DB_OPCODE(CODE, ...) && CODE,

src/backend/StackMachine.hpp
@@ -48,7 +48,7 @@ struct StackMachine
     void emit(const Expr &expr);
     void emit(const cnf::CNF &cnf);
 
-    tuple_type operator()(const tuple_type &t = tuple_type());
+    const tuple_type & operator()(const tuple_type &t = tuple_type());
 
     /* The following macros are used to automatically generate methods to emit a particular opcode.  For example, for
      * the opcode `Pop`, we will define a function `emit_Pop()`, that appends the `Pop` opcode to the current opcode
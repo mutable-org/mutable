08db17dbb2d4e38addeb654d81d9febc99e0cb4c
Immanuel Haffner <haffner.immanuel@gmail.com>
2020-01-13 11:37:53 +0100

    Update V8 backend to match the imported memory of the WASM module.

--- src/backend/V8Backend.cpp
+++ src/backend/V8Backend.cpp
@@ -1,5 +1,6 @@
 #include "V8Backend.hpp"
 
+#include "v8-internal.h"
 
 using namespace db;
 
@@ -46,34 +47,55 @@ void V8Backend::execute(const WASMModule &module)
             ).ToLocalChecked();
     free(binary_addr);
 
-    /* Create V8 strings. */
-    auto str_WebAssembly = v8::String::NewFromUtf8(isolate_, "WebAssembly").ToLocalChecked();
-    auto str_Instance    = v8::String::NewFromUtf8(isolate_, "Instance").ToLocalChecked();
-    auto str_export      = v8::String::NewFromUtf8(isolate_, "exports").ToLocalChecked();
-    auto str_main        = v8::String::NewFromUtf8(isolate_, "main").ToLocalChecked();
+#define V8STR(str) ( v8::String::NewFromUtf8(isolate_, (str)).ToLocalChecked() )
 
     /* Get the WebAssembly instance class prototype. */
-    auto instance_class = context->Global()
-        ->Get(context, str_WebAssembly).ToLocalChecked().As<v8::Object>()
-        ->Get(context, str_Instance).ToLocalChecked().As<v8::Object>();
-    insist(not instance_class.IsEmpty());
+    auto web_assembly_class = context->Global()->Get(context, V8STR("WebAssembly")).ToLocalChecked().As<v8::Object>();
 
-    /* Create a WebAssembly instance. */
-    args_t instance_args { v8_wasm_module };
-    auto instance = instance_class->CallAsConstructor(context, 1, instance_args).ToLocalChecked().As<v8::Object>();
+    /* Create a WebAssembly memory object. */
+    auto wasm_memory_class = web_assembly_class->Get(context, V8STR("Memory")).ToLocalChecked().As<v8::Object>();
+    auto memory_params_object = v8::Object::New(isolate_);
+    memory_params_object->Set(context, V8STR("initial"), v8::Int32::New(isolate_, 1));
+    memory_params_object->Set(context, V8STR("maximum"), v8::Int32::New(isolate_, 1));
+    args_t memory_args { memory_params_object };
+    auto wasm_memory = wasm_memory_class->CallAsConstructor(context, 1, memory_args).ToLocalChecked().As<v8::Object>();
+    insist(not wasm_memory.IsEmpty());
+
+    /* Create host memory. */
+    auto host_memory = new int32_t[1024];
+    for (int32_t i = 0; i != 1024; ++i)
+        host_memory[i] = i;
+    auto store = v8::ArrayBuffer::NewBackingStore(host_memory, 1024 * sizeof(int32_t), [](void* data, size_t, void*) { delete[] reinterpret_cast<int32_t*>(data); }, nullptr);
+    auto buffer = v8::ArrayBuffer::New(isolate_, std::move(store));
+    wasm_memory->Set(context, V8STR("buffer"), buffer); /// XXX: the underlying buffer is not replaced
+
+    /* Create the import object for instantiating the WebAssembly module. */
+    auto host_object = v8::Object::New(isolate_);
+    host_object->Set(context, V8STR("mem"), wasm_memory);
+    auto import_object = v8::Object::New(isolate_);
+    import_object->Set(context, V8STR("env"), host_object);
+
+    /* Create a WebAssembly instance object. */
+    auto instance_class = web_assembly_class->Get(context, V8STR("Instance")).ToLocalChecked().As<v8::Object>();
+    insist(not instance_class.IsEmpty());
+    args_t instance_args { v8_wasm_module, import_object };
+    auto instance = instance_class->CallAsConstructor(context, 2, instance_args).ToLocalChecked().As<v8::Object>();
     insist(not instance.IsEmpty());
 
     /* Get the exports of the created WebAssembly instance. */
-    auto exports = instance->Get(context, str_export).ToLocalChecked().As<v8::Object>();
+    auto exports = instance->Get(context, V8STR("exports")).ToLocalChecked().As<v8::Object>();
     insist(not exports.IsEmpty());
 
-    /* Get exported function `main` from the exports. */
-    auto main = exports->Get(context, str_main).ToLocalChecked().As<v8::Function>();
-    insist(not main.IsEmpty());
+    /* Get exported function `run` from the exports. */
+    auto run = exports->Get(context, V8STR("run")).ToLocalChecked().As<v8::Function>();
+    insist(not run.IsEmpty());
 
-    /* Invoke the exported function `main` of the module. */
+    /* Invoke the exported function `run` of the module. */
     std::cerr << "Executing the module...";
-    args_t args{};
-    main->Call(context, context->Global(), 0, args);
+    args_t args{ };
+    auto result = run->Call(context, context->Global(), 0, args).ToLocalChecked().As<v8::Object>();
+    insist(not result.IsEmpty());
     std::cerr << " Finished.\n";
+
+    std::cout << "Result is " << *v8::String::Utf8Value(isolate_, v8::Local<v8::String>::Cast(result)) << std::endl;
 }

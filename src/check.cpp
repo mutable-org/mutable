2b6391bdf6f48101fbbd5b05d6800335a69fe405
Marcel Maltry <marcel.maltry@bigdata.uni-saarland.de>
2020-02-19 14:27:40 +0100

    Add support for multiple input files in 'check'.
    'check' now supports an similar input scheme like 'shell' where multiple
    input files and `-` can be provided.  Various other adjustments:
    - Use `get_options()` to store command line arguments.
    - Add `--quiet` command line argument.

--- src/check.cpp
+++ src/check.cpp
@@ -1,3 +1,4 @@
+#include "globals.hpp"
 #include "lex/Lexer.hpp"
 #include "lex/Token.hpp"
 #include "lex/TokenType.hpp"
@@ -20,90 +21,97 @@ using namespace db;
 void usage(std::ostream &out, const char *name)
 {
     out << "Performs semantic analysis of the input.\n"
-        << "USAGE:\n\t" << name << " <FILE>"
-        << "\n\t" << name << " -"
+        << "USAGE:\n\t" << name << " [<FILE>...]"
         << std::endl;
 }
 
 int main(int argc, const char **argv)
 {
+    /*----- Parse command line arguments. ----------------------------------------------------------------------------*/
     ArgParser AP;
 #define ADD(TYPE, VAR, INIT, SHORT, LONG, DESCR, CALLBACK)\
-    TYPE VAR = INIT;\
+    VAR = INIT;\
     {\
         std::function<void(TYPE)> callback = CALLBACK;\
         AP.add(SHORT, LONG, VAR, DESCR, callback);\
     }
-    ADD(bool, show_help, false,             /* Type, Var, Init  */
-        "-h", "--help",                     /* Short, Long      */
-        "prints this help message",         /* Description      */
-        [&](bool) { show_help = true; });   /* Callback         */
-    ADD(bool, color, false,                 /* Type, Var, Init  */
-        nullptr, "--color",                 /* Short, Long      */
-        "use colors",                       /* Description      */
-        [&](bool) { color = true; });       /* Callback         */
-    ADD(bool, ast, false,                   /* Type, Var, Init  */
-        nullptr, "--ast",                   /* Short, Long      */
-        "print AST",                        /* Description      */
-        [&](bool) { ast = true; });         /* Callback         */
+    ADD(bool, get_options().show_help, false,               /* Type, Var, Init  */
+        "-h", "--help",                                     /* Short, Long      */
+        "prints this help message",                         /* Description      */
+        [&](bool) { get_options().show_help = true; });     /* Callback         */
+    ADD(bool, get_options().has_color, false,               /* Type, Var, Init  */
+        nullptr, "--color",                                 /* Short, Long      */
+        "use colors",                                       /* Description      */
+        [&](bool) { get_options().has_color = true; });     /* Callback         */
+    ADD(bool, get_options().ast, false,                     /* Type, Var, Init  */
+        nullptr, "--ast",                                   /* Short, Long      */
+        "print AST",                                        /* Description      */
+        [&](bool) { get_options().ast = true; });           /* Callback         */
+    ADD(bool, get_options().quiet, false,                   /* Type, Var, Init  */
+        "-q", "--quiet",                                    /* Short, Long      */
+        "work in quiet mode",                               /* Description      */
+        [&](bool) { get_options().quiet = true; });         /* Callback         */
 #undef ADD
     AP.parse_args(argc, argv);
 
-    if (show_help) {
+    if (get_options().show_help) {
         usage(std::cout, argv[0]);
         std::cout << "WHERE\n";
         AP.print_args(stdout);
         std::exit(EXIT_SUCCESS);
     }
 
-    if (AP.args().size() != 1) {
-        usage(std::cerr, argv[0]);
-        std::cerr << "WHERE\n";
-        AP.print_args(stderr);
-        std::exit(EXIT_FAILURE);
-    }
+    auto args = AP.args();
+    if (args.empty())
+        args.push_back("-"); // start in interactive mode
 
-    const char *filename = AP.args()[0];
     std::istream *in;
-    if (streq(filename, "-")) {
-        /* read from stdin */
-        in = &std::cin;
-    } else {
-        /* read from file */
-        in = new std::ifstream(filename, std::ios_base::in);
-    }
 
-    if (in->fail()) {
-        if (in == &std::cin)
-            std::cerr << "Failed to open stdin: ";
-        else
-            std::cerr << "Failed to open the file '" << filename << "': ";
-        std::cerr << strerror(errno) << std::endl;
-    }
+    /* Create the diagnostics object. */
+    Diagnostic diag(get_options().has_color, std::cout, std::cerr);
 
-    Catalog &C = Catalog::Get();
-    Diagnostic diag(color, std::cout, std::cerr);
-    Lexer lexer(diag, C.get_pool(), filename, *in);
-    Parser parser(lexer);
-    Sema sema(diag);
     bool sema_error = false;
+    Catalog &C = Catalog::Get();
 
-    while (parser.token()) {
-        auto stmt = parser.parse();
-        if (diag.num_errors()) {
-            diag.clear();
-            delete stmt;
-            continue;
+    /* Process all the inputs. */
+    for (auto filename : args) {
+        if (streq(filename, "-")) {
+            /* read from stdin */
+            in = &std::cin;
+        } else {
+            /* read from file */
+            in = new std::ifstream(filename, std::ios_base::in);
         }
-        sema(*stmt);
-        if (ast) stmt->dump(std::cout);
-        sema_error = sema_error or diag.num_errors();
-        diag.clear(); // clear sema errors
-        delete stmt;
-    }
 
-    if (in != &std::cin)
-        delete in;
+        if (in->fail()) {
+            if (in == &std::cin)
+                std::cerr << "Failed to open stdin: ";
+            else
+                std::cerr << "Failed to open the file '" << filename << "': ";
+            std::cerr << strerror(errno) << std::endl;
+        }
+
+        Lexer lexer(diag, C.get_pool(), filename, *in);
+        Parser parser(lexer);
+        Sema sema(diag);
+
+        while (parser.token()) {
+            auto stmt = parser.parse();
+            if (diag.num_errors()) {
+                diag.clear();
+                delete stmt;
+                continue;
+            }
+            sema(*stmt);
+            if (get_options().ast) stmt->dump(std::cout);
+            sema_error = sema_error or diag.num_errors();
+            diag.clear(); // clear sema errors
+            delete stmt;
+        }
+
+        if (in != &std::cin)
+            delete in;
+    }
 
     std::exit(sema_error ? EXIT_FAILURE : EXIT_SUCCESS);
 }

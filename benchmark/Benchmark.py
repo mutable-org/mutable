089b4ebf67cc6e262855c5add383fc9f3ac6c337
Immanuel Haffner <haffner.immanuel@gmail.com>
2020-03-26 09:38:39 +0100

    Change relative path to 'style.css'.

--- benchmark/Benchmark.py
+++ benchmark/Benchmark.py
@@ -202,7 +202,7 @@ if __name__ == '__main__':
                              href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css',
                              integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh',
                              crossorigin='anonymous')
-            doc.stag('link', rel='stylesheet', type='text/css', href='benchmark/style.css')
+            doc.stag('link', rel='stylesheet', type='text/css', href='style.css')
             doc.stag('link', rel='stylesheet',
                              href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css',
                              integrity='sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=',

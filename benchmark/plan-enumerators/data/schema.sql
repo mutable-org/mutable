b4304ac9da97d6c2a584b18e49a187f3676ab486
Joris Nix <joris.nix@bigdata.uni-saarland.de>
2020-04-01 18:39:03 +0200

    Revise `PlanEnumerator` benchmarks.
    Rename 'optimizer' suite into 'plan-enumerators'.  Remove
    'benchmark/optimizer/gen.py' script because we do not actually generate
    any data and the schema is fixed.  Therefore, add the 'schema.sql' file.

---
+++ benchmark/plan-enumerators/data/schema.sql
@@ -0,0 +1,122 @@
+CREATE DATABASE optimizer;
+USE optimizer;
+
+CREATE TABLE T0
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T1
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T2
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T3
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T4
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T5
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T6
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T7
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T8
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T9
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T10
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T11
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T12
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T13
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T14
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T15
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T16
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T17
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T18
+(
+    key INT(4),
+    fkey INT(4)
+);
+
+CREATE TABLE T19
+(
+    key INT(4),
+    fkey INT(4)
+);

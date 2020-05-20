1fcabb674d951edd61911b7d97406532d444c8f9
Immanuel Haffner <haffner.immanuel@gmail.com>
2020-05-20 14:52:13 +0200

    Add benchmarks of grouping performance.

---
+++ benchmark/operators/group_by_i32_distinct_postgresql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define path to PostgreSQL CLI
+POSTGRESQL=psql
+
+{ ${POSTGRESQL} -U postgres | grep 'Time' | cut -d ' ' -f 2; } << EOF
+DROP DATABASE IF EXISTS benchmark_tmp;
+CREATE DATABASE benchmark_tmp;
+\c benchmark_tmp
+CREATE TABLE Distinct_i32 ( id INT, n1 INT, n10 INT, n100 INT, n1000 INT, n10000 INT, n100000 INT);
+\copy Distinct_i32 FROM 'benchmark/operators/data/Distinct_i32.csv' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT 1 FROM Distinct_i32 GROUP BY n10;
+SELECT 1 FROM Distinct_i32 GROUP BY n100;
+SELECT 1 FROM Distinct_i32 GROUP BY n1000;
+SELECT 1 FROM Distinct_i32 GROUP BY n10000;
+SELECT 1 FROM Distinct_i32 GROUP BY n100000;
+EOF
---
+++ benchmark/operators/group_by_i32_multiple_duckdb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define path to DuckDB CLI
+DUCKDB=duckdb_cli
+
+{ ${DUCKDB} | grep 'Run Time' | cut -d ' ' -f 4 | awk '{print $1 * 1000;}'; } << EOF
+CREATE TABLE Distinct_i32 ( id INT, n1 INT, n10 INT, n100 INT, n1000 INT, n10000 INT, n100000 INT);
+COPY Distinct_i32 FROM 'benchmark/operators/data/Distinct_i32.csv' ( HEADER );
+.timer on
+SELECT 1 FROM Distinct_i32 GROUP BY n10000;
+SELECT 1 FROM Distinct_i32 GROUP BY n10000, n1000;
+SELECT 1 FROM Distinct_i32 GROUP BY n10000, n1000, n100;
+SELECT 1 FROM Distinct_i32 GROUP BY n10000, n1000, n100, n10;
+EOF

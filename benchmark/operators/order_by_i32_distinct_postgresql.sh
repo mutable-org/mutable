fcf7cbf96aac6b8dba2e197fa439ac68ae2ecba7
Immanuel Haffner <haffner.immanuel@gmail.com>
2020-05-20 16:21:14 +0200

    Remove case in benchmark where DuckDB crashes.
    When there is only a single (distinct) value to order by, DuckDB's
    sorting algorithm does crazy recursion.  Eventually, the stack takes up
    to much space and the process is terminated by the kernel.

--- benchmark/operators/order_by_i32_distinct_postgresql.sh
+++ benchmark/operators/order_by_i32_distinct_postgresql.sh
@@ -10,7 +10,6 @@ CREATE DATABASE benchmark_tmp;
 CREATE TABLE Distinct_i32 ( id INT, n1 INT, n10 INT, n100 INT, n1000 INT, n10000 INT, n100000 INT);
 \copy Distinct_i32 FROM 'benchmark/operators/data/Distinct_i32.csv' WITH DELIMITER ',' CSV HEADER;
 \timing on
-SELECT id FROM Distinct_i32 ORDER BY n1;
 SELECT id FROM Distinct_i32 ORDER BY n10;
 SELECT id FROM Distinct_i32 ORDER BY n100;
 SELECT id FROM Distinct_i32 ORDER BY n1000;

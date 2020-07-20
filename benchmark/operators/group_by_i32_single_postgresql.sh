bf09664a908850037557b19ac430a9931cf0aea9
Immanuel Haffner <haffner.immanuel@gmail.com>
2020-07-20 10:08:42 +0200

    Improve some benchmarks of PostgresQL.
    Instead of exploiting the `WHERE`-clause to achieve a certain scale
    factor, just import as many rows from the CSV as necessary.  This avoids
    the additional overhead of filtering.

--- benchmark/operators/group_by_i32_single_postgresql.sh
+++ benchmark/operators/group_by_i32_single_postgresql.sh
@@ -2,23 +2,78 @@
 
 # Define path to PostgreSQL CLI
 POSTGRESQL=psql
+CSV="benchmark/operators/data/Distinct_i32.csv"
+NUM=$(($(wc -l ${CSV} | cut -f 1 -d ' ') - 1))
 
 { ${POSTGRESQL} -U postgres | grep 'Time' | cut -d ' ' -f 2; } << EOF
 DROP DATABASE IF EXISTS benchmark_tmp;
 CREATE DATABASE benchmark_tmp;
 \c benchmark_tmp
 CREATE TABLE Distinct_i32 ( id INT, n1 INT, n10 INT, n100 INT, n1000 INT, n10000 INT, n100000 INT);
-\copy Distinct_i32 FROM 'benchmark/operators/data/Distinct_i32.csv' WITH DELIMITER ',' CSV HEADER;
+
+\copy Distinct_i32 FROM PROGRAM 'head -n 0 ${CSV}' WITH DELIMITER ',' CSV HEADER;
 \timing on
-SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 WHERE n1000 < -2147483647 GROUP BY n1000) AS T;
-SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 WHERE n1000 < -1717986918 GROUP BY n1000) AS T;
-SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 WHERE n1000 < -1288490188 GROUP BY n1000) AS T;
-SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 WHERE n1000 <  -858993459 GROUP BY n1000) AS T;
-SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 WHERE n1000 <  -429496729 GROUP BY n1000) AS T;
-SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 WHERE n1000 <           0 GROUP BY n1000) AS T;
-SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 WHERE n1000 <   429496729 GROUP BY n1000) AS T;
-SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 WHERE n1000 <   858993459 GROUP BY n1000) AS T;
-SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 WHERE n1000 <  1288490188 GROUP BY n1000) AS T;
-SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 WHERE n1000 <  1717986918 GROUP BY n1000) AS T;
-SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 WHERE n1000 <  2147483647 GROUP BY n1000) AS T;
+SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 GROUP BY n1000) AS T;
+\timing off
+DELETE FROM Distinct_i32;
+
+\copy Distinct_i32 FROM PROGRAM 'head -n $((${NUM} * 1 / 10)) ${CSV}' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 GROUP BY n1000) AS T;
+\timing off
+DELETE FROM Distinct_i32;
+
+\copy Distinct_i32 FROM PROGRAM 'head -n $((${NUM} * 2 / 10)) ${CSV}' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 GROUP BY n1000) AS T;
+\timing off
+DELETE FROM Distinct_i32;
+
+\copy Distinct_i32 FROM PROGRAM 'head -n $((${NUM} * 3 / 10)) ${CSV}' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 GROUP BY n1000) AS T;
+\timing off
+DELETE FROM Distinct_i32;
+
+\copy Distinct_i32 FROM PROGRAM 'head -n $((${NUM} * 4 / 10)) ${CSV}' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 GROUP BY n1000) AS T;
+\timing off
+DELETE FROM Distinct_i32;
+
+\copy Distinct_i32 FROM PROGRAM 'head -n $((${NUM} * 5 / 10)) ${CSV}' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 GROUP BY n1000) AS T;
+\timing off
+DELETE FROM Distinct_i32;
+
+\copy Distinct_i32 FROM PROGRAM 'head -n $((${NUM} * 6 / 10)) ${CSV}' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 GROUP BY n1000) AS T;
+\timing off
+DELETE FROM Distinct_i32;
+
+\copy Distinct_i32 FROM PROGRAM 'head -n $((${NUM} * 7 / 10)) ${CSV}' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 GROUP BY n1000) AS T;
+\timing off
+DELETE FROM Distinct_i32;
+
+\copy Distinct_i32 FROM PROGRAM 'head -n $((${NUM} * 8 / 10)) ${CSV}' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 GROUP BY n1000) AS T;
+\timing off
+DELETE FROM Distinct_i32;
+
+\copy Distinct_i32 FROM PROGRAM 'head -n $((${NUM} * 9 / 10)) ${CSV}' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 GROUP BY n1000) AS T;
+\timing off
+DELETE FROM Distinct_i32;
+
+\copy Distinct_i32 FROM PROGRAM 'head -n ${NUM} ${CSV}' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT COUNT(*) FROM (SELECT 1 FROM Distinct_i32 GROUP BY n1000) AS T;
+\timing off
+DELETE FROM Distinct_i32;
 EOF

0a8f07dc862df62877d250650c5fda6d49698d13
Immanuel Haffner <haffner.immanuel@gmail.com>
2021-01-04 15:53:45 +0100

    Make redundant check in `DPsub` an `insist()`.

--- src/IR/PlanEnumerator.cpp
+++ src/IR/PlanEnumerator.cpp
@@ -248,9 +248,9 @@ void DPsub::operator()(const QueryGraph &G, const CostFunction &cf, PlanTable &P
             ++inner_counter;
 #endif
             Subproblem S2 = S - S1; // = S \ S1;
+            insist(M.is_connected(S1, S2), "implied by S inducing a connected subgraph");
             if (not M.is_connected(S1)) continue; // not connected? -> skip
             if (not M.is_connected(S2)) continue; // not connected? -> skip
-            if (not M.is_connected(S1, S2)) continue; // not connected? -> skip
 #if PE_COUNTER
             ++csg_cmp_pair_counter;
 #endif

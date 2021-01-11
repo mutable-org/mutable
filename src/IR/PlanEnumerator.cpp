9f5e55fc2648c78810f8d06a0e05e23b73e90d1d
Immanuel Haffner <haffner.immanuel@gmail.com>
2021-01-11 12:03:31 +0100

    Remove unused code for `DummyPlanEnumerator`.

--- src/IR/PlanEnumerator.cpp
+++ src/IR/PlanEnumerator.cpp
@@ -30,63 +30,6 @@ std::unique_ptr<PlanEnumerator> PlanEnumerator::Create(PlanEnumerator::kind_t ki
 }
 
 /*======================================================================================================================
- * DummyPlanEnumerator
- *====================================================================================================================*/
-
-/** Computes an arbitrary join order (deterministically). */
-struct DummyPlanEnumerator final : PlanEnumerator
-{
-    void operator()(const QueryGraph &G, const CostFunction &CF, PlanTable &PT) const override;
-};
-
-void DummyPlanEnumerator::operator()(const QueryGraph&, const CostFunction&, PlanTable&) const
-{
-#if 0
-    while (not worklist.empty()) {
-        auto G = worklist.back();
-        worklist.pop_back();
-        auto &order = map[G];
-
-        std::unordered_set<const DataSource*> frontier;
-        if (not G->sources().empty())
-            frontier.insert(*G->sources().begin());
-        std::unordered_set<const Join*> joins(G->joins().begin(), G->joins().end()); ///< set of available joins
-        std::unordered_set<const DataSource*> used; ///< set of already joined data sources
-
-        while (not frontier.empty()) {
-            /* Pick the next relation from the frontier. */
-            auto r = *frontier.begin();
-            used.insert(r);
-            order.push_back(r); // emit
-
-            /* Check if relation is subquery. */
-            if (auto q = cast<const Query>(r))
-                worklist.push_back(q->query_graph());
-
-            /* Apply all possible joins. */
-            std::vector<const Join*> joins_used;
-            for (auto j : joins) { // TODO apply smaller joins first to get better plans
-                if (subset(j->sources(), used)) {
-                    joins_used.push_back(j);
-                    order.push_back(j); // emit
-                }
-            }
-            for (auto j : joins_used) joins.erase(j); // erase the used joins so every join is used exactly once
-
-            /* Update frontier.  Explore 'r' by adding all its unexplored neighbors to the frontier. */
-            frontier.erase(r);
-            for (auto j : r->joins()) {
-                for (auto n : j->sources()) {
-                    if (used.count(n) == 0)
-                        frontier.insert(n);
-                }
-            }
-        }
-    }
-#endif
-}
-
-/*======================================================================================================================
  * DPsize
  *====================================================================================================================*/
 

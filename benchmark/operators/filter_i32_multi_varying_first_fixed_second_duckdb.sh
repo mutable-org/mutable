ea6e407dc3d744bf7dede4699e7d057a9c2fb185
Immanuel Haffner <haffner.immanuel@gmail.com>
2021-01-13 18:09:10 +0100

    Add filter benchmark.
    Add filter benchmark with conjunction of two predicates.

---
+++ benchmark/operators/filter_i32_multi_varying_first_fixed_second_duckdb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Define path to DuckDB CLI
+DUCKDB=duckdb_cli
+
+{ ${DUCKDB} | grep 'Run Time' | cut -d ' ' -f 4 | awk '{print $1 * 1000;}'; } << EOF
+CREATE TABLE Attribute_i32 ( id INT, val INT);
+COPY Attribute_i32 FROM 'benchmark/operators/data/Attribute_i32.csv' ( HEADER );
+.timer on
+SELECT COUNT(*) FROM Attribute_i32 WHERE val < -2147483647 AND val <           0;
+SELECT COUNT(*) FROM Attribute_i32 WHERE val < -1717986918 AND val > -2147483647;
+SELECT COUNT(*) FROM Attribute_i32 WHERE val < -1288490188 AND val > -1717986918;
+SELECT COUNT(*) FROM Attribute_i32 WHERE val <  -858993459 AND val > -1288490188;
+SELECT COUNT(*) FROM Attribute_i32 WHERE val <  -429496729 AND val >  -858993459;
+SELECT COUNT(*) FROM Attribute_i32 WHERE val <           0 AND val >  -429496729;
+SELECT COUNT(*) FROM Attribute_i32 WHERE val <   429496729 AND val >           0;
+SELECT COUNT(*) FROM Attribute_i32 WHERE val <   858993459 AND val >   429496729;
+SELECT COUNT(*) FROM Attribute_i32 WHERE val <  1288490188 AND val >   858993459;
+SELECT COUNT(*) FROM Attribute_i32 WHERE val <  1717986918 AND val >  1288490188;
+SELECT COUNT(*) FROM Attribute_i32 WHERE val <  2147483647 AND val >  1717986918;
+EOF

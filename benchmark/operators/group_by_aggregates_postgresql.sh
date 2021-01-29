d585b11fe777cffb8c1759d74bf0dbd135da2b14
Immanuel Haffner <haffner.immanuel@gmail.com>
2021-01-29 23:29:48 +0100

    Add benchmark to evaluate aggregation.

---
+++ benchmark/operators/group_by_aggregates_postgresql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define path to PostgreSQL CLI
+POSTGRESQL=psql
+
+{ ${POSTGRESQL} -U postgres | grep 'Time' | cut -d ' ' -f 2; } << EOF
+DROP DATABASE IF EXISTS benchmark_tmp;
+CREATE DATABASE benchmark_tmp;
+\c benchmark_tmp
+CREATE TABLE Distinct_i32 ( id INT, n1 INT, n10 INT, n100 INT, n1000 INT, n10000 INT, n100000 INT);
+\copy Distinct_i32 FROM 'benchmark/operators/data/Distinct_i32.csv' WITH DELIMITER ',' CSV HEADER;
+\timing on
+SELECT MIN(n100000) FROM Distinct_i32 GROUP BY n10;
+SELECT MIN(n100000), MIN(n10000) FROM Distinct_i32 GROUP BY n10;
+SELECT MIN(n100000), MIN(n10000), MIN(n1000) FROM Distinct_i32 GROUP BY n10;
+SELECT MIN(n100000), MIN(n10000), MIN(n1000), MIN(n100) FROM Distinct_i32 GROUP BY n10;
+EOF

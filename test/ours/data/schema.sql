c32b1867dbfbef1e7f9b169e30480f127b2c6e8c
Immanuel Haffner <haffner.immanuel@gmail.com>
2021-02-22 14:30:54 +0100

    Add `HAS HEADER` in test schema to silence warning

--- test/ours/data/schema.sql
+++ test/ours/data/schema.sql
@@ -29,7 +29,7 @@ CREATE TABLE D (
     rdatetime DATETIME
 );
 
-IMPORT INTO R DSV "test/ours/data/R.csv" SKIP HEADER;
-IMPORT INTO S DSV "test/ours/data/S.csv" SKIP HEADER;
-IMPORT INTO T DSV "test/ours/data/T.csv" SKIP HEADER;
-IMPORT INTO D DSV "test/ours/data/D.csv" SKIP HEADER;
+IMPORT INTO R DSV "test/ours/data/R.csv" HAS HEADER SKIP HEADER;
+IMPORT INTO S DSV "test/ours/data/S.csv" HAS HEADER SKIP HEADER;
+IMPORT INTO T DSV "test/ours/data/T.csv" HAS HEADER SKIP HEADER;
+IMPORT INTO D DSV "test/ours/data/D.csv" HAS HEADER SKIP HEADER;

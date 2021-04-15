21bcb9e9a8027449db29e5f753b872346a2753ce
Immanuel Haffner <haffner.immanuel@gmail.com>
2021-04-15 11:35:18 +0200

    Add TPC-H Q6 benchmarks for DuckDB and HyPer.

---
+++ benchmark/tpc-h/q6_duckdb.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+CSV="benchmark/tpc-h/data/lineitem.tbl"
+NUM_ROWS=$(wc -l "${CSV}" | cut -f 1 -d ' ')
+NUM_ROWS=$((NUM_ROWS-1))
+
+# Define path to DuckDB CLI
+DUCKDB=duckdb_cli
+
+trap 'exit' INT
+{ ${DUCKDB} | grep 'Run Time' | cut -d ' ' -f 4 | awk '{print $1 * 1000;}'; } << EOF
+CREATE TABLE Lineitem (
+    l_orderkey      INT,
+    l_partkey       INT,
+    l_suppkey       INT,
+    l_linenumber    INT,
+    l_quantity      DECIMAL(10,2),
+    l_extendedprice DECIMAL(10,2),
+    l_discount      DECIMAL(10,2),
+    l_tax           DECIMAL(10,2),
+    l_returnflag    CHAR(1),
+    l_linestatus    CHAR(1),
+    l_shipdate      DATE,
+    l_commitdate    DATE,
+    l_receiptdate   DATE,
+    l_shipinstruct  CHAR(25),
+    l_shipmode      CHAR(10),
+    l_comment       CHAR(44)
+);
+COPY Lineitem FROM '${CSV}' ( HEADER, DELIMITER '|' );
+.timer on
+SELECT
+        SUM(l_extendedprice * l_discount) AS revenue
+FROM
+        Lineitem
+WHERE
+        l_shipdate >= DATE '1994-01-01'
+        AND l_shipdate < DATE '1995-01-01'
+        AND l_discount > 0.05 AND l_discount < 0.07
+        AND l_quantity < 24;
+SELECT
+        SUM(l_extendedprice * l_discount) AS revenue
+FROM
+        Lineitem
+WHERE
+        l_shipdate >= DATE '1994-01-01'
+        AND l_shipdate < DATE '1995-01-01'
+        AND l_discount > 0.05 AND l_discount < 0.07
+        AND l_quantity < 24;
+SELECT
+        SUM(l_extendedprice * l_discount) AS revenue
+FROM
+        Lineitem
+WHERE
+        l_shipdate >= DATE '1994-01-01'
+        AND l_shipdate < DATE '1995-01-01'
+        AND l_discount > 0.05 AND l_discount < 0.07
+        AND l_quantity < 24;
+EOF
---
+++ benchmark/tpc-h/q6_hyper.py
@@ -0,0 +1,60 @@
+#!/bin/env python3
+
+import numpy
+import time
+from tableauhyperapi import HyperProcess, Telemetry, Connection, CreateMode, NOT_NULLABLE, NULLABLE, SqlType, \
+        TableDefinition, Inserter, escape_name, escape_string_literal, HyperException, TableName
+
+if __name__ == '__main__':
+    with HyperProcess(telemetry=Telemetry.DO_NOT_SEND_USAGE_DATA_TO_TABLEAU) as hyper:
+        with Connection(endpoint=hyper.endpoint, database='benchmark.hyper', create_mode=CreateMode.CREATE_AND_REPLACE) as connection:
+            columns = [
+                TableDefinition.Column('l_orderkey',        SqlType.int(), NOT_NULLABLE),
+                TableDefinition.Column('l_partkey',         SqlType.int(), NOT_NULLABLE),
+                TableDefinition.Column('l_suppkey',         SqlType.int(), NOT_NULLABLE),
+                TableDefinition.Column('l_linenumber',      SqlType.int(), NOT_NULLABLE),
+                TableDefinition.Column('l_quantity',        SqlType.numeric(10,2), NOT_NULLABLE),
+                TableDefinition.Column('l_extendedprice',   SqlType.numeric(10,2), NOT_NULLABLE),
+                TableDefinition.Column('l_discount',        SqlType.numeric(10,2), NOT_NULLABLE),
+                TableDefinition.Column('l_tax',             SqlType.numeric(10,2), NOT_NULLABLE),
+                TableDefinition.Column('l_returnflag',      SqlType.char(1), NOT_NULLABLE),
+                TableDefinition.Column('l_linestatus',      SqlType.char(1), NOT_NULLABLE),
+                TableDefinition.Column('l_shipdate',        SqlType.date(), NOT_NULLABLE),
+                TableDefinition.Column('l_commitdate',      SqlType.date(), NOT_NULLABLE),
+                TableDefinition.Column('l_receiptdate',     SqlType.date(), NOT_NULLABLE),
+                TableDefinition.Column('l_shipinstruct',    SqlType.char(25), NOT_NULLABLE),
+                TableDefinition.Column('l_shipmode',        SqlType.char(10), NOT_NULLABLE),
+                TableDefinition.Column('l_comment',         SqlType.char(44), NOT_NULLABLE),
+            ]
+
+            table_def = TableDefinition(
+                table_name='Lineitem',
+                columns=columns
+            )
+            query = f'''
+SELECT
+        SUM(l_extendedprice * l_discount) AS revenue
+FROM
+        {table_def.table_name}
+WHERE
+        l_shipdate >= date '1994-01-01'
+        AND l_shipdate < date '1995-01-01'
+        AND l_quantity < 24;'''
+
+            times = list()
+            for i in range(3):
+                connection.catalog.create_table(table_def)
+                connection.execute_command(f'COPY {table_def.table_name} FROM \'benchmark/tpc-h/data/lineitem.tbl\' WITH DELIMITER \'|\' CSV')
+
+                begin = time.time_ns()
+                with connection.execute_query(query) as result:
+                    for row in result:
+                        pass
+                end = time.time_ns()
+                times.append(end - begin)
+
+                connection.execute_command(f'DELETE FROM {table_def.table_name}')
+                connection.execute_command(f'DROP TABLE {table_def.table_name}')
+
+            for t in times:
+                print(t / 1e6) # in milliseconds

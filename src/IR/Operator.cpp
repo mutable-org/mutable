82782c37e1e795b8b22484f2de952d0793652312
Immanuel Haffner <haffner.immanuel@gmail.com>
2021-05-10 14:07:59 +0200

    Implement `Operator::dot()` with new visitors.
    Get rid of former `OperatorDot` entirely and replace it with an
    implementation of `Operator::dot()` using our new visitor design.

--- src/IR/Operator.cpp
+++ src/IR/Operator.cpp
@@ -1,6 +1,4 @@
-#include "mutable/IR/Operator.hpp"
-
-#include "OperatorDot.hpp"
+#include <mutable/IR/Operator.hpp>
 
 
 using namespace m;
@@ -84,8 +82,108 @@ std::ostream & m::operator<<(std::ostream &out, const Operator &op) {
 
 void Operator::dot(std::ostream &out) const
 {
-    OperatorDot D(out);
-    D(*this);
+    constexpr const char * const GRAPH_TYPE = "digraph";
+    constexpr const char * const EDGE = " -> ";
+    out << GRAPH_TYPE << " plan\n{\n"
+        << "    forcelabels=true;\n"
+        << "    overlap=false;\n"
+        << "    rankdir=BT;\n"
+        << "    graph [compound=true];\n"
+        << "    graph [fontname = \"DejaVu Sans\"];\n"
+        << "    node [fontname = \"DejaVu Sans\"];\n"
+        << "    edge [fontname = \"DejaVu Sans\"];\n";
+
+#define q(X) '"' << X << '"' // quote
+#define id(X) q(std::hex << &X << std::dec) // convert virtual address to identifier
+    visit(overloaded {
+        [&out](const ScanOperator &op) {
+            out << "    " << id(op) << " [label=<<B>" << html_escape(op.store().table().name) << "</B>>];\n";
+        },
+        [&out](const FilterOperator &op) {
+            out << "    " << id(op) << " [label=<<B>σ</B><SUB><FONT COLOR=\"0.0 0.0 0.25\" POINT-SIZE=\"10\">"
+                << html_escape(to_string(op.filter()))
+                << "</FONT></SUB>>];\n"
+                << "    " << id(*op.child(0)) << EDGE << id(op) << ";\n";
+        },
+        [&out](const JoinOperator &op) {
+            out << "    " << id(op) << " [label=<<B>⋈</B><SUB><FONT COLOR=\"0.0 0.0 0.25\" POINT-SIZE=\"10\">"
+                << html_escape(to_string(op.predicate()))
+                << "</FONT></SUB>>];\n";
+
+            for (auto c : op.children())
+                out << "    " << id(*c) << EDGE << id(op) << ";\n";
+        },
+        [&out](const ProjectionOperator &op) {
+            out << id(op) << " [label=<<B>π</B><SUB><FONT COLOR=\"0.0 0.0 0.25\" POINT-SIZE=\"10\">";
+            if (op.is_anti()) out << '*';
+            const auto &P = op.projections();
+            for (auto it = P.begin(); it != P.end(); ++it) {
+                if (it != P.begin()) out << ", ";
+                out << *it->first;
+                if (it->second)
+                    out << " AS " << it->second;
+            }
+            out << "</FONT></SUB>>];\n";
+
+            if (not op.children().empty())
+                out << id(*op.child(0)) << EDGE << id(op) << ";\n";
+        },
+        [&out](const LimitOperator &op) {
+            out << "    " << id(op) << " [label=<<B>λ</B><SUB><FONT COLOR=\"0.0 0.0 0.25\" POINT-SIZE=\"10\">"
+                << op.limit();
+            if (op.offset()) out << ", " << op.offset();
+            out << "</FONT></SUB>>];\n"
+                << "    " << id(*op.child(0)) << EDGE << id(op) << ";\n";
+        },
+        [&out](const GroupingOperator &op) {
+            out << "    " << id(op) << " [label=<<B>γ</B><SUB><FONT COLOR=\"0.0 0.0 0.25\" POINT-SIZE=\"10\">";
+
+            const auto &G = op.group_by();
+            const auto &A = op.aggregates();
+
+            for (auto it = G.begin(); it != G.end(); ++it) {
+                if (it != G.begin()) out << ", ";
+                out << **it;
+            }
+
+            if (G.size() and A.size()) out << ", ";
+
+            for (auto it = A.begin(); it != A.end(); ++it) {
+                if (it != A.begin()) out << ", ";
+                out << **it;
+            }
+
+            out << "</FONT></SUB>>];\n"
+                << "    " << id(*op.child(0)) << EDGE << id(op) << ";\n";
+        },
+        [&out](const AggregationOperator &op) {
+            out << "    " << id(op) << " [label=<<B>Γ</B><SUB><FONT COLOR=\"0.0 0.0 0.25\" POINT-SIZE=\"10\">";
+            const auto &A = op.aggregates();
+            for (auto it = A.begin(); it != A.end(); ++it) {
+                if (it != A.begin()) out << ", ";
+                out << **it;
+            }
+
+            out << "</FONT></SUB>>];\n"
+                << "    " << id(*op.child(0)) << EDGE << id(op) << ";\n";
+        },
+        [&out](const SortingOperator &op) {
+            out << "    " << id(op) << " [label=<<B>ω</B><SUB><FONT COLOR=\"0.0 0.0 0.25\" POINT-SIZE=\"10\">";
+
+            const auto &O = op.order_by();
+            for (auto it = O.begin(); it != O.end(); ++it) {
+                if (it != O.begin()) out << ", ";
+                out << *it->first << ' ' << (it->second ? "ASC" : "DESC");
+            }
+
+            out << "</FONT></SUB>>];\n"
+                << "    " << id(*op.child(0)) << EDGE << id(op) << ";\n";
+        },
+        [](auto&&) { /* nothing to be done */ }
+    }, *this, tag<ConstPostOrderOperatorVisitor>());
+#undef id
+#undef q
+    out << "}\n";
 }
 
 void Operator::dump(std::ostream &out) const { out << *this << std::endl; }

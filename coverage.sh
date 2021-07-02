708560ab93da8fc24bda119f831821eaa90bd502
Immanuel Haffner <haffner.immanuel@gmail.com>
2021-07-02 10:15:42 +0200

    Fix bugs in coverage report generation.
    - With a recent modification of the top-level `CMakeLists.txt`, the
    `--coverage` flag set by the `coverage.sh` script was not properly
    passed to the compiler.
    - The generated `gcov.sh` script -- used to let `lcov` use `llvm-cov
    gcov` -- could not be invoked by its relative path.  We now use the
    absolute path instead.
    - An experimental change is to *not* delete the `.gc*` files --
    generated as byproduct of compilation with `--coverage` -- anymore and
    to *not* clean the build directory anymore.  This hopefully speeds up
    coverage report generation.  A potential negative side effect may be
    that updates don't make it into the report.  We have to see...

--- coverage.sh
+++ coverage.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
 
+trap "exit" INT
+
 # Make coverage build
 mkdir -p build/coverage
 cd build/coverage
 
-find . -iname '*.gcov' -exec rm {} \;
-find . -iname '*.gcno' -exec rm {} \;
-find . -iname '*.gcda' -exec rm {} \;
+# find . -iname '*.gcov' -exec rm {} \;
+# find . -iname '*.gcno' -exec rm {} \;
+# find . -iname '*.gcda' -exec rm {} \;
 
 cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Debug \
     -DCMAKE_C_COMPILER=clang \
     -DCMAKE_CXX_COMPILER=clang++ \
-    -DCMAKE_C_FLAGS_DEBUG=--coverage \
-    -DCMAKE_CXX_FLAGS_DEBUG=--coverage \
+    -DCMAKE_COMPILE_FLAGS=--coverage \
     ../..
-ninja clean
-ninja
+# ninja clean
+ninja -v
 
 # Produce gcov alias for llvm-cov gcov
-echo -e '#!/bin/bash\nllvm-cov gcov "$@"' > gcov.sh
-chmod a+x gcov.sh
+GCOV="$(pwd)/gcov.sh"
+echo -e '#!/bin/bash\nllvm-cov gcov "$@"' > "${GCOV}"
+chmod a+x "${GCOV}"
 
 # Cleanup lcov
-lcov --quiet --gcov-tool ./gcov.sh --directory src --rc lcov_branch_coverage=1 --zerocounters
-lcov --quiet --gcov-tool ./gcov.sh --directory unittest --rc lcov_branch_coverage=1 --zerocounters
+lcov --quiet --gcov-tool "${GCOV}" --directory src --rc lcov_branch_coverage=1 --zerocounters
+lcov --quiet --gcov-tool "${GCOV}" --directory unittest --rc lcov_branch_coverage=1 --zerocounters
 # Create baseline to make sure untouched files show up in the report
-lcov --quiet --gcov-tool ./gcov.sh --directory src --capture --initial --output-file src.base
-lcov --quiet --gcov-tool ./gcov.sh --directory unittest --capture --initial --output-file unittest.base
+lcov --quiet --gcov-tool "${GCOV}" --directory src --capture --initial --output-file src.base
+lcov --quiet --gcov-tool "${GCOV}" --directory unittest --capture --initial --output-file unittest.base
 # Run tests
 bin/unittest --durations yes --reporter junit --out catch.xml \[core\]
 # Capture lcov counters and generate report
-lcov --quiet --gcov-tool ./gcov.sh --directory src --rc lcov_branch_coverage=1 \
+lcov --quiet --gcov-tool "${GCOV}" --directory src --rc lcov_branch_coverage=1 \
      --capture --output-file src.capture
-lcov --quiet --gcov-tool ./gcov.sh --directory unittest --rc lcov_branch_coverage=1 \
+lcov --quiet --gcov-tool "${GCOV}" --directory unittest --rc lcov_branch_coverage=1 \
      --capture --output-file unittest.capture
 # Add baseline counters
-lcov --quiet --gcov-tool ./gcov.sh --directory src --rc lcov_branch_coverage=1 \
+lcov --quiet --gcov-tool "${GCOV}" --directory src --rc lcov_branch_coverage=1 \
      --add-tracefile src.base \
      --add-tracefile src.capture \
      --add-tracefile unittest.base \
      --add-tracefile unittest.capture \
      --output-file cov.total
 # Filter collected data to final coverage report
-lcov --quiet --gcov-tool ./gcov.sh --directory src --rc lcov_branch_coverage=1 \
+lcov --quiet --gcov-tool "${GCOV}" --directory src --rc lcov_branch_coverage=1 \
      --remove cov.total '*unittest*' '/usr/include/*' '*third-party/*' \
      --output-file cov.info
 
@@ -58,4 +60,4 @@ rm unittest.base
 rm unittest.capture
 rm cov.total
 rm cov.info
-rm gcov.sh
+rm "${GCOV}"

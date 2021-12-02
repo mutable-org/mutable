2a654753152ec81ff96711d038f7631832e2d8eb
Immanuel Haffner <haffner.immanuel@gmail.com>
2021-12-02 16:13:31 +0100

    Formatting.

--- include/mutable/catalog/SimpleCostFunction.hpp
+++ include/mutable/catalog/SimpleCostFunction.hpp
@@ -5,14 +5,14 @@
 
 namespace m {
 
-struct SimpleCostFunction : CostFunction {
-    SimpleCostFunction() : CostFunction() {}
-
+struct SimpleCostFunction : CostFunction
+{
     double calculate_filter_cost(const QueryGraph &G, const PlanTable &PT, const CardinalityEstimator &CE,
                                  const Subproblem &sub, const cnf::CNF &condition) const override;
 
     double calculate_join_cost(const QueryGraph &G, const PlanTable &PT, const CardinalityEstimator &CE,
-                               const Subproblem &left, const Subproblem &right, const cnf::CNF &condition) const override;
+                               const Subproblem &left, const Subproblem &right,
+                               const cnf::CNF &condition) const override;
 
     double calculate_grouping_cost(const QueryGraph &G, const PlanTable &PT, const CardinalityEstimator &CE,
                                    const Subproblem &sub, const std::vector<const Expr*> &group_by) const override;

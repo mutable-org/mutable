794b9686eda4b13b8f1226bcd807d81f24bfe39d
Immanuel Haffner <haffner.immanuel@gmail.com>
2022-04-18 20:02:53 +0200

    Add script to collect #vertices generated.

---
+++ eval-information-value.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Trap on CTRL-C / SIGINT
+trap "exit 1" SIGINT
+
+set -o pipefail -o nounset
+
+source planner-configs.sh
+
+BINDIR=build/release/bin
+BIN=${BINDIR}/shell
+
+ORDERED_PLANNERS=(
+    "BU-A*-zero"
+    "BU-A*-GOO"
+    "TD-A*-zero"
+    "TD-A*-sum"
+    "TD-A*-GOO"
+)
+NUM_REPETITIONS=5
+
+echo "planner,topology,relations,iteration,vertices"
+for T in {chain,cycle,star,clique};
+do
+    for I in $(seq 1 ${NUM_REPETITIONS});
+    do
+        for N in {5..10}; do
+            NAME="$T-${N}"
+
+            python3 querygen.py -t $T -n ${N} > /dev/null
+            build/release/bin/cardinality_gen \
+                --seed ${RANDOM} \
+                "${NAME}.schema.sql" \
+                "${NAME}.query.sql" \
+            > "${NAME}.cardinalities.json"
+
+            for PLANNER in "${ORDERED_PLANNERS[@]}";
+            do
+                if [ ! -v 'PLANNER_CONFIGS[$PLANNER]' ];
+                then
+                    >&2 echo "ERROR: no configuration found for ${PLANNER}"
+                    continue
+                fi
+                PLANNER_CONFIG=${PLANNER_CONFIGS[$PLANNER]}
+
+                echo -n "${PLANNER},${T},${N},${I},"
+                timeout --signal=SIGTERM --kill-after=7s 5s ${BIN} \
+                    --quiet --dryrun \
+                    --plan-table-las \
+                    --cardinality-estimator InjectionCardinalityEstimator \
+                    --use-cardinality-file "${NAME}.cardinalities.json" \
+                    ${PLANNER_CONFIG} \
+                    "${NAME}.schema.sql" \
+                    "${NAME}.query.sql" \
+                    2>&1 | ack --nocolor '^States generated:' | cut -d ':' -f 2 | tr -d ' '
+            done
+
+            rm -f "${NAME}.schema.sql" "${NAME}.query.sql" "${NAME}.cardinalities.json"
+        done
+    done
+done

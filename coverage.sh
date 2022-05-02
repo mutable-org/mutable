16726f137cbdf4895a2217618353bc8e3733cea0
Immanuel Haffner <haffner.immanuel@gmail.com>
2022-05-02 08:29:55 +0200

    Fix coverage script.
    - Simplify the script by avoiding duplicate calls to `lcov` that can be
      merged by specifying *two* directories.
    - Properly exclude external sources (`/usr/include/` et al)

--- coverage.sh
+++ coverage.sh
@@ -4,16 +4,38 @@ set -e -o pipefail
 
 trap "exit" INT
 
+BASE_DIR=$(pwd)
+
 # Ignore List: Remove data for files that should be ignored in the final coverage report
-IGNORE_LIST=('*QueryGraph2SQL.*' '*PDDL.*' '*TrainedCostFunction.*' '*CostModel.*' '*src/storage/Linearization.cpp')
+IGNORE_LIST=(
+    # subdirs
+    '*unittest*'
+    '*third-party/*'
+    # individual files
+    '*QueryGraph2SQL.*'
+    '*PDDL.*'
+    '*TrainedCostFunction.*'
+    '*CostModel.*'
+    '*src/storage/Linearization.cpp'
+)
 
 # Make coverage build
 mkdir -p build/coverage
 cd build/coverage
 
-# find . -iname '*.gcov' -exec rm {} \;
-# find . -iname '*.gcno' -exec rm {} \;
-# find . -iname '*.gcda' -exec rm {} \;
+set -x
+
+# Produce gcov alias for llvm-cov gcov
+GCOV="$(pwd)/gcov.sh"
+echo -e '#!/bin/bash\nllvm-cov gcov "$@"' > "${GCOV}"
+chmod a+x "${GCOV}"
+
+LCOV_FLAGS="\
+    --quiet \
+    --gcov-tool ${GCOV} \
+    --base-directory ${BASE_DIR} \
+    --no-external \
+    --rc lcov_branch_coverage=1"
 
 cmake -G Ninja \
     -DCMAKE_BUILD_TYPE=Debug \
@@ -26,49 +48,28 @@ cmake -G Ninja \
 ninja gitversion
 ninja -v unittest
 
-# Produce gcov alias for llvm-cov gcov
-GCOV="$(pwd)/gcov.sh"
-echo -e '#!/bin/bash\nllvm-cov gcov "$@"' > "${GCOV}"
-chmod a+x "${GCOV}"
-
 # Cleanup lcov
-lcov --quiet --gcov-tool "${GCOV}" --directory src --rc lcov_branch_coverage=1 --zerocounters
-lcov --quiet --gcov-tool "${GCOV}" --directory unittest --rc lcov_branch_coverage=1 --zerocounters
+lcov ${LCOV_FLAGS} --zerocounters --directory src --directory unittest
 # Create baseline to make sure untouched files show up in the report
-lcov --quiet --gcov-tool "${GCOV}" --directory src --capture --initial --output-file src.base
-lcov --quiet --gcov-tool "${GCOV}" --directory unittest --capture --initial --output-file unittest.base
+lcov ${LCOV_FLAGS} --capture --initial --directory src --directory unittest --output-file base.info
+# lcov ${LCOV_FLAGS} --capture --initial --output-file unittest.base
 # Run tests
-bin/unittest --durations yes --reporter junit --out catch.xml \[core\]
+bin/unittest --durations yes --reporter junit --out catch.xml '[core]'
 # Capture lcov counters and generate report
-lcov --quiet --gcov-tool "${GCOV}" --directory src --rc lcov_branch_coverage=1 \
-     --capture --output-file src.capture
-lcov --quiet --gcov-tool "${GCOV}" --directory unittest --rc lcov_branch_coverage=1 \
-     --capture --output-file unittest.capture
+lcov ${LCOV_FLAGS} --capture --directory src --directory unittest --output-file test.info
+# lcov ${LCOV_FLAGS} --capture --directory unittest --output-file unittest.capture
 # Add baseline counters
-lcov --quiet --gcov-tool "${GCOV}" --directory src --rc lcov_branch_coverage=1 \
-     --add-tracefile src.base \
-     --add-tracefile src.capture \
-     --add-tracefile unittest.base \
-     --add-tracefile unittest.capture \
-     --output-file cov.total
+lcov ${LCOV_FLAGS} --directory src --directory unittest --add-tracefile base.info --add-tracefile test.info --output-file total.info
 # Filter collected data to final coverage report
-lcov --quiet --gcov-tool "${GCOV}" --directory src --rc lcov_branch_coverage=1 \
-     --remove cov.total '*unittest*' '/usr/include/*' '*third-party/*' \
-     --output-file cov.info
-# Apply Ignore List: Remove data for files that should be ignored in the final coverage report
-lcov --quiet --gcov-tool "${GCOV}" --directory src --rc lcov_branch_coverage=1 \
-     --remove cov.info "${IGNORE_LIST[@]}" \
-     --output-file cov.info
+lcov ${LCOV_FLAGS} --directory src --directory unittest --remove total.info "${IGNORE_LIST[@]}" --output-file coverage.info
 
 # Generate HTML output
 rm -fr html
-genhtml --branch-coverage --rc lcov_branch_coverage=1 cov.info -o html
+genhtml --branch-coverage --rc lcov_branch_coverage=1 coverage.info -o html
 
 # Cleanup
-rm src.base
-rm src.capture
-rm unittest.base
-rm unittest.capture
-rm cov.total
-rm cov.info
+rm base.info
+rm test.info
+rm total.info
+rm coverage.info
 rm "${GCOV}"

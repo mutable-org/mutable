48f3347ca1563cbbaed39d0757c21fd0d62b0ecc
Immanuel Haffner <haffner.immanuel@gmail.com>
2022-05-07 14:33:04 +0200

    Fix comparison of signed and unsigned in `MemoryTest`.

--- unittest/util/MemoryTest.cpp
+++ unittest/util/MemoryTest.cpp
@@ -26,7 +26,7 @@ TEST_CASE("memory::Memory/c'tor", "[core][util][memory]")
 TEST_CASE("memory::LinearAllocator", "[core][util][memory]")
 {
     const std::size_t PAGE_SIZE = get_pagesize();
-    const std::size_t INTS_PER_PAGE = PAGE_SIZE / sizeof(int);
+    const std::size_t INTS_PER_PAGE = PAGE_SIZE / sizeof(unsigned);
 
     LinearAllocator A;
 
@@ -42,7 +42,7 @@ TEST_CASE("memory::LinearAllocator", "[core][util][memory]")
         CHECK(A.offset() == PAGE_SIZE);
 
         /* Write page of ints. */
-        auto pi = mem0.as<int*>();
+        auto pi = mem0.as<unsigned*>();
         for (std::size_t i = 0; i != INTS_PER_PAGE; ++i)
             pi[i] = i;
 
@@ -93,7 +93,7 @@ TEST_CASE("memory::LinearAllocator", "[core][util][memory]")
     SECTION("map to address space")
     {
         auto mem = A.allocate(2 * PAGE_SIZE); // 2 pages
-        auto p_mem = mem.as<int*>();
+        auto p_mem = mem.as<unsigned*>();
         for (std::size_t i = 0; i != 2 * INTS_PER_PAGE; ++i)
             p_mem[i] = i;
 
@@ -101,13 +101,13 @@ TEST_CASE("memory::LinearAllocator", "[core][util][memory]")
             AddressSpace vm(4 * PAGE_SIZE); // 4 pages
             mem.map(PAGE_SIZE, PAGE_SIZE, vm, 0); // map mem page 1 to vm page 0
             {
-                auto p_vm = reinterpret_cast<int*>(vm.addr());
+                auto p_vm = reinterpret_cast<unsigned*>(vm.addr());
                 for (std::size_t i = 0; i != INTS_PER_PAGE; ++i)
                     REQUIRE(p_vm[i] == INTS_PER_PAGE + i);
             }
             mem.map(PAGE_SIZE, 0, vm, PAGE_SIZE); // map mem page 0 to vm page 1
             {
-                auto p_vm = reinterpret_cast<int*>(vm.addr());
+                auto p_vm = reinterpret_cast<unsigned*>(vm.addr());
                 for (std::size_t i = 0; i != INTS_PER_PAGE; ++i)
                     REQUIRE(p_vm[i] == INTS_PER_PAGE + i);
                 for (std::size_t i = 0; i != INTS_PER_PAGE; ++i)

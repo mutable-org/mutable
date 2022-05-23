ce91ef74ee02a0ecb058e02e658730eb85ed9c98
Immanuel Haffner <haffner.immanuel@gmail.com>
2022-05-23 13:01:28 +0200

    Fix script to eval num states generated.

--- eval-states-generated.sh
+++ eval-states-generated.sh
@@ -16,7 +16,6 @@ BIN=build/debug_shared/bin/shell
 MIN_CARDINALITY=10
 MAX_CARDINALITY=10000
 
-
 for T in {chain,cycle,star,clique};
 do
     for N in {5,10,15};
@@ -45,31 +44,44 @@ do
             2>&1 | ack --nocolor '\d+ CCPs' | cut -d ',' -f 2 | cut -d ' '  -f 2
 
 
-        if [ "${NAME}" != "clique-15" ];
-        then
-            # Dijkstra
-            echo -n "Dijkstra,$T,$N,"
+            # Dijkstra↑
+            echo -n "Dijkstra↑,$T,$N,"
             ${BIN} \
                 --quiet --dryrun \
                 --plan-table-las \
                 --cardinality-estimator Injected \
                 --use-cardinality-file "${NAME}.cardinalities.json" \
                 --statistics \
-                --plan-enumerator HeuristicSearch --hs-vertex SubproblemsArray --hs-heuristic zero --hs-search AStar \
+                --plan-enumerator HeuristicSearch --hs-vertex SubproblemsArray --hs-expand BottomUpComplete --hs-heuristic zero --hs-search AStar \
+                "${NAME}.schema.sql" \
+                "${NAME}.query.sql" \
+                2>&1 | ack 'Vertices generated:' | cut -d ':' -f 2 | tr -d ' '
+
+        if [ "${NAME}" != "clique-15" ];
+        then
+            # Dijkstra↓
+            echo -n "Dijkstra↓,$T,$N,"
+            ${BIN} \
+                --quiet --dryrun \
+                --plan-table-las \
+                --cardinality-estimator Injected \
+                --use-cardinality-file "${NAME}.cardinalities.json" \
+                --statistics \
+                --plan-enumerator HeuristicSearch --hs-vertex SubproblemsArray --hs-expand TopDownComplete --hs-heuristic zero --hs-search AStar \
                 "${NAME}.schema.sql" \
                 "${NAME}.query.sql" \
                 2>&1 | ack 'Vertices generated:' | cut -d ':' -f 2 | tr -d ' '
         fi
 
         # A*
-        echo -n "A*_sum,$T,$N,"
+        echo -n "A*↓ + h_sum,$T,$N,"
         ${BIN} \
             --quiet --dryrun \
             --plan-table-las \
             --cardinality-estimator Injected \
             --use-cardinality-file "${NAME}.cardinalities.json" \
             --statistics \
-            --plan-enumerator HeuristicSearch --hs-vertex SubproblemsArray --hs-heuristic sum --hs-search AStar \
+            --plan-enumerator HeuristicSearch --hs-vertex SubproblemsArray --hs-expand TopDownComplete --hs-heuristic sum --hs-search AStar \
             "${NAME}.schema.sql" \
             "${NAME}.query.sql" \
             2>&1 | ack 'Vertices generated:' | cut -d ':' -f 2 | tr -d ' '

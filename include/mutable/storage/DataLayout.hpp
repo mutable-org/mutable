1538490179a81e17db10b6842f93f78361657c27
Immanuel Haffner <haffner.immanuel@gmail.com>
2022-11-11 08:01:32 +0100

    Add documentation for `DataLayout`.

--- include/mutable/storage/DataLayout.hpp
+++ include/mutable/storage/DataLayout.hpp
@@ -17,6 +17,7 @@ namespace storage {
 
 struct ConstDataLayoutVisitor;
 
+/** Models how data is laid out in a linear address space. */
 struct DataLayout
 {
     /*----- Types ----------------------------------------------------------------------------------------------------*/
@@ -48,22 +49,28 @@ struct DataLayout
                                                  const level_info_stack_t &levels,
                                                  uint64_t inode_offset_in_bits)>;
 
+    ///> an abstract node in the recursive data layout model
     struct Node
     {
         virtual ~Node() = 0;
 
+        ///> returns the number of tuples represented by an instance of this node
         virtual size_type num_tuples() const = 0;
 
         virtual void accept(ConstDataLayoutVisitor &v) const = 0;
     };
 
+    /** The `Leaf` represents exactly one attribue.  It holds the `Type` of the `Attribute` together with a unique
+     * index.  With the unique index it is possible to associate the `Attribute` to this `Leaf`. */
     struct Leaf : Node
     {
         friend struct DataLayout;
         friend struct INode;
 
         private:
+        ///> the `Type` of the attribute represented by this `Leaf`
         const m::Type *type_;
+        ///> an index that must be unique within the entire `DataLayout`
         size_type idx_;
 
         Leaf(const m::Type *type, size_type idx) : type_(type), idx_(idx) { }
@@ -79,10 +86,13 @@ struct DataLayout
         void accept(ConstDataLayoutVisitor &v) const override;
     };
 
+    /** An internal node of the recursive data layout model.  It holds one or oore child `Node`s and stores the offset
+     * and stride for each child.  The offset and stride are relative within this `INode`. */
     struct INode : Node
     {
         friend struct DataLayout;
 
+        ///> a child `Node` and its relative offset and stride within the containing `INode`
         struct child_t
         {
             std::unique_ptr<Node> ptr;
@@ -121,6 +131,7 @@ struct DataLayout
         };
 
         private:
+        ///> the child `Node`s of this `INode`
         std::vector<child_t> children_;
         ///> the number of tuples that fit into one instance of this `INode`
         size_type num_tuples_;
@@ -133,15 +144,22 @@ struct DataLayout
 
         public:
         size_type num_tuples() const override { return num_tuples_; }
+        ///> returns the number of child `Node`s of this `INode`
         size_type num_children() const { return children_.size(); }
 
+        /** Creates a `Leaf` and adds it as a child to this `INode`.  The created `Leaf` will have the given, relative
+         * \p offset_in_bits and \p stride_in_bits within this `INode`. */
         Leaf & add_leaf(const m::Type *type, size_type idx, uint64_t offset_in_bits, uint64_t stride_in_bits);
+        /** Creates an `INode` and adds it as a child to this `INode`.  The created `INode` represents \p num_tuples
+         * many tuples and will have the given, relative \p offset_in_bits and \p stride_in_bits. */
         INode & add_inode(size_type num_tuples, uint64_t offset_in_bits, uint64_t stride_in_bits);
 
+        ///> returns a reference to the \p idx -th child
         const child_t & operator[](size_type idx) const {
             M_insist(idx < children_.size(), "index out of bounds");
             return children_[idx];
         }
+        ///> returns a reference to the \p idx -th child; throws `m::invalid_argument` if \p idx is out of bounds
         const child_t & at(size_type idx) const {
             if (idx >= children_.size()) throw m::invalid_argument("index out of bounds");
             return children_[idx];
@@ -168,20 +186,33 @@ struct DataLayout
 
     /*----- Methods --------------------------------------------------------------------------------------------------*/
     public:
+    /** Create a new `DataLayout` for laying out \p num_tuples many tuples in linear memory.  If \p num_tuples is zero,
+     *  this `DataLayout` lays out an indefinite sequence of tuples. */
     DataLayout(size_type num_tuples = 0) : inode_(num_tuples) { }
 
+    ///> returns `true` iff this `DataLayout` lays out a finite sequence of tuples
     bool is_finite() const { return inode_.num_tuples() != 0; }
+    ///> returns the number of tuples laid out by this `DataLayout`; must not be called when `not is_finite()`
     size_type num_tuples() const {
         M_insist(is_finite(), "infinite data layouts have no number of tuples");
         return inode_.num_tuples();
     }
+    ///> return the stride (in bits) of the single child of the `DataLayout`
     uint64_t stride_in_bits() const { M_insist(bool(*this), "no child set"); return inode_[0].stride_in_bits; }
+    ///> returns a reference to the single child of this `DataLayout`
     const Node & child() const { M_insist(bool(*this), "no child set"); return *inode_[0].ptr; }
 
+    ///> returns `true` iff this `DataLayout` has a child
     operator bool() const { return inode_.num_children() == 1; }
+    ///> returns a reference to the `DataLayout`'s internal `INode` object
     explicit operator const INode&() const { return inode_; }
 
+    /** Creates a `Leaf` and adds it as a child to this `DataLayout`'s internal `INode`.  The created `Leaf` will have
+     * the given, relative \p offset_in_bits and \p stride_in_bits within the internal `INode`. */
     Leaf & add_leaf(const m::Type *type, size_type idx, uint64_t stride_in_bits);
+    /** Creates an `INode` and adds it as a child to this `DataLayout`'s internal `INode`.  The created `INode`
+     * represents \p num_tuples many tuples and will have the given, relative \p offset_in_bits and \p stride_in_bits.
+     */
     INode & add_inode(size_type num_tuples, uint64_t stride_in_bits);
 
     void accept(ConstDataLayoutVisitor &v) const;

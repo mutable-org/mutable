c152f756c75c54fe62aa7cf2ef5082904ee4593a
Luca Gretscher <s8lugret@stud.uni-saarland.de>
2022-11-30 11:27:23 +0100

    Add comment to `quicksort()`.

--- src/backend/WasmAlgo.hpp
+++ src/backend/WasmAlgo.hpp
@@ -12,6 +12,9 @@ namespace wasm {
  * sorting
  *====================================================================================================================*/
 
+/** Sorts the buffer \p buffer using the quicksort algorithm and a branchless binary partition algorithm.  The
+ * ordering is specified by \p order where the first element is the expression to order on and the second element is
+ * `true` iff ordering should be performed ascending. */
 template<bool IsGlobal>
 void quicksort(const Buffer<IsGlobal> &buffer, const std::vector<SortingOperator::order_type> &order);
 

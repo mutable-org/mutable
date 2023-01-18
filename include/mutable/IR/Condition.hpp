bdaf928b0faff60a68b4770b4c2d5877a064540d
Luca Gretscher <s8lugret@stud.uni-saarland.de>
2023-01-18 17:07:06 +0100

    Rewrite `Condition`s and `ConditionSet`.

--- include/mutable/IR/Condition.hpp
+++ include/mutable/IR/Condition.hpp
@@ -10,93 +10,265 @@
 namespace m {
 
 
-struct Condition
-{
-    virtual ~Condition() { };
+/*======================================================================================================================
+ * helper structure
+ *====================================================================================================================*/
 
-    /** Checks whether this `Condition` is implied by \p other. */
-    virtual bool implied_by(const Condition &other) = 0;
-
-    /** Inform this `Condition` that the `Identifier` \p old_id was renamed to \p new_id. */
-    virtual void rename(Schema::Identifier old_id, Schema::Identifier new_id) = 0;
-};
-
-template<typename Property>
+template<typename Property, bool Ordered>
 struct ConditionPropertyMap
 {
     private:
-    std::vector<std::pair<Schema::Identifier, Property>> attrs;
+    struct IdentifierHash
+    {
+        std::size_t operator()(Schema::Identifier id) const {
+            return murmur3_64(intptr_t(id.prefix) ^ intptr_t(id.name));
+        }
+    };
+
+    using map_t = std::conditional_t<Ordered, std::vector<std::pair<Schema::Identifier, Property>>,
+                                              std::unordered_map<Schema::Identifier, Property, IdentifierHash>>;
+
+    map_t attrs;
 
     public:
     void add(Schema::Identifier id, Property P) {
-        auto it = std::find(attrs.begin(), attrs.end(), id);
-        if (id == attrs.end())
-            attrs.emplace_back(id, std::move(P));
-        else
-            throw std::logic_error("identifier already in use");
+        auto it = find(id);
+        if (it == cend()) {
+            if constexpr (Ordered)
+                attrs.emplace_back(id, std::move(P));
+            else
+                attrs.emplace_hint(it, id, std::move(P));
+        } else {
+            throw invalid_argument("identifier already in use");
+        }
     }
 
-    void rename(Schema::Identifier old_id, Schema::Identifier new_id) {
-        /*----- Check whether we actually rename the ID. */
-        if (old_id == new_id) [[unlikely]] return; // nothing to be done
-
-        /*----- Check whether the new ID is not yet in use. -----*/
-        {
-            auto it = std::find_if(attrs.begin(), attrs.end(), [new_id](const auto &e) -> bool {
-                return e.first == new_id;
-            });
-
-            if (it != attrs.end())
-                throw std::logic_error("new id already in use");
+    void merge(ConditionPropertyMap &other) {
+        if constexpr (Ordered) {
+            auto pred = [this](const auto &p) -> bool { return this->find(p.first) == this->cend(); };
+            if (not std::all_of(other.cbegin(), other.cend(), pred))
+                throw invalid_argument("identifier already in use");
+            this->attrs.insert(this->cend(), other.begin(), other.end());
+        } else {
+            this->attrs.merge(other.attrs);
+            if (not other.attrs.empty())
+                throw invalid_argument("identifier already in use");
         }
+    }
 
-        /*----- Find the entry with the old ID. -----*/
-        auto it = std::find_if(attrs.begin(), attrs.end(), [old_id](const auto &e) -> bool {
-            return e.first == old_id;
-        });
-        if (it == attrs.end())
-            throw std::logic_error("old id not found");
+    auto find(Schema::Identifier id) const {
+        if constexpr (Ordered) {
+            auto pred = [&id](const auto &e) -> bool { return e.first == id; };
+            auto it = std::find_if(cbegin(), cend(), pred);
+            if (it != cend() and std::find_if(std::next(it), cend(), pred) != cend())
+                throw invalid_argument("duplicate identifier, lookup ambiguous");
+            return it;
+        } else {
+            return attrs.find(id);
+        }
+    }
 
-        /*----- Rename. -----*/
-        it->first = new_id;
+    void project_and_rename(const std::vector<std::pair<Schema::Identifier, Schema::Identifier>> &old2new) {
+        auto old = std::exchange(*this, ConditionPropertyMap());
+        for (auto [old_id, new_id] : old2new) {
+            /*----- Try to find the entry with the old ID. -----*/
+            auto it = old.find(old_id);
+            if (it != old.cend()) {
+                /*----- Insert found entry again with the new ID. -----*/
+                this->add(new_id, std::move(it->second));
+            }
+        }
     }
 
     auto begin() { return attrs.begin(); }
     auto end() { return attrs.end(); }
-    auto begin() const { return attrs.begin(); }
-    auto end() const { return attrs.end(); }
+    auto begin() const { return attrs.cbegin(); }
+    auto end() const { return attrs.cend(); }
     auto cbegin() const { return begin(); }
     auto cend() const { return end(); }
+
+    bool operator==(const ConditionPropertyMap &other) const { return this->attrs == other.attrs; }
+};
+
+template<typename Property>
+using ConditionPropertyUnorderedMap = ConditionPropertyMap<Property, false>;
+template<typename Property>
+using ConditionPropertyOrderedMap = ConditionPropertyMap<Property, true>;
+
+
+/*======================================================================================================================
+ * Condition
+ *====================================================================================================================*/
+
+struct Condition
+{
+    friend struct ConditionSet;
+
+    virtual ~Condition() { };
+
+    private:
+    /** Creates and returns a *deep copy* of `this`. */
+    virtual std::unique_ptr<Condition> clone() const = 0;
+
+    public:
+    /** Checks whether this `Condition` is implied by \p other. */
+    virtual bool implied_by(const Condition &other) const = 0;
+
+    /** Inform this `Condition` that `Identifier`s  were simultaneously projected and renamed according to the
+     * mapping \p old2new, i.e. afterwards, this `Condition` does only consist of `Identifier`s contained as second
+     * element of each pair in the mapping. */
+    virtual void project_and_rename(const std::vector<std::pair<Schema::Identifier, Schema::Identifier>> &old2new) = 0;
+
+    virtual bool operator==(const Condition &other) const = 0;
+    bool operator!=(const Condition &other) const { return not operator==(other); }
+};
+
+struct Unsatisfiable final : Condition
+{
+    explicit Unsatisfiable() = default;
+    explicit Unsatisfiable(const Unsatisfiable&) = default;
+    Unsatisfiable(Unsatisfiable&&) = default;
+
+    private:
+    std::unique_ptr<Condition> clone() const override { return std::make_unique<Unsatisfiable>(); }
+
+    public:
+
+    bool implied_by(const Condition&) const override { return false; }
+
+    void project_and_rename(const std::vector<std::pair<Schema::Identifier, Schema::Identifier>>&) override { }
+
+    bool operator==(const Condition &o) const override { return is<const Unsatisfiable>(&o); }
 };
 
 struct Sortedness final : Condition
 {
-    enum Order { O_ASC, O_DESC };
+    enum Order { O_ASC, O_DESC, O_UNDEF /* undefined for single tuple results */ };
 
-    ConditionPropertyMap<Order> orders;
+    using order_t = ConditionPropertyOrderedMap<Order>;
 
-    bool implied_by(const Condition &o) override {
+    private:
+    order_t orders_;
+
+    public:
+    explicit Sortedness(order_t orders) : orders_(orders) { }
+
+    Sortedness() = default;
+    explicit Sortedness(const Sortedness&) = default;
+    Sortedness(Sortedness&&) = default;
+
+    private:
+    std::unique_ptr<Condition> clone() const override { return std::make_unique<Sortedness>(orders_); }
+
+    public:
+    order_t & orders() { return orders_; }
+    const order_t & orders() const { return orders_; }
+
+    bool implied_by(const Condition &o) const override {
         auto other = cast<const Sortedness>(&o);
         if (not other) return false;
-        auto other_it = other->orders.cbegin();
 
-        for (auto this_it = this->orders.cbegin(); this_it != this->orders.cend(); ++this_it) {
-            for (;;) { // find current attribute of `this` in `other`
-                if (other_it == other->orders.cend())
-                    return false; // attribute not found
-                if (other_it->first == this_it->first) // attribute found
-                    break;
-                ++other_it; // proceed
-            }
-            M_insist(this_it->first == other_it->first);
+        for (auto this_it = this->orders_.begin(); this_it != this->orders_.end(); ++this_it) {
+            const auto other_it = other->orders_.find(this_it->first);
+            if (other_it == other->orders_.cend())
+                return false; // attribute not found
+            if (other_it->second == O_UNDEF or this_it->second == O_UNDEF)
+                continue; // sort order undefined implies attribute order does not matter
+            if (std::distance(this_it, this->orders_.begin()) != std::distance(other_it, other->orders_.cbegin()))
+                return false; // different attribute order
             if (this_it->second != other_it->second)
                 return false; // opposite sort order
         }
         return true;
     }
 
-    virtual void rename(Schema::Identifier old_id, Schema::Identifier new_id) override {
-        orders.rename(old_id, new_id);
+    void project_and_rename(const std::vector<std::pair<Schema::Identifier, Schema::Identifier>> &old2new) override {
+        orders_.project_and_rename(old2new);
+    }
+
+    bool operator==(const Condition &o) const override {
+        auto other = cast<const Sortedness>(&o);
+        if (not other) return false;
+        return this->orders_ == other->orders_;
+    }
+};
+
+struct SIMD_Width final : Condition
+{
+    using width_t = ConditionPropertyUnorderedMap<unsigned>;
+
+    private:
+    width_t simd_widths_; ///< as log_2
+
+    public:
+    explicit SIMD_Width(width_t simd_width) : simd_widths_(simd_width) { }
+
+    SIMD_Width() = default;
+    explicit SIMD_Width(const SIMD_Width&) = default;
+    SIMD_Width(SIMD_Width&&) = default;
+
+    private:
+    std::unique_ptr<Condition> clone() const override { return std::make_unique<SIMD_Width>(simd_widths_); }
+
+    public:
+    width_t & simd_widths() { return simd_widths_; }
+    const width_t & simd_widths() const { return simd_widths_; }
+
+    bool implied_by(const Condition &o) const override {
+        auto other = cast<const SIMD_Width>(&o);
+        if (not other) return false;
+
+        for (auto &this_entry : this->simd_widths_) {
+            auto it = other->simd_widths_.find(this_entry.first);
+            if (it == other->simd_widths_.cend())
+                return false; // attribute not found
+            if (this_entry.second != it->second)
+                return false; // different vector size
+        }
+        return true;
+    }
+
+    void project_and_rename(const std::vector<std::pair<Schema::Identifier, Schema::Identifier>> &old2new) override {
+        simd_widths_.project_and_rename(old2new);
+    }
+
+    bool operator==(const Condition &o) const override {
+        auto other = cast<const SIMD_Width>(&o);
+        if (not other) return false;
+        return this->simd_widths_ == other->simd_widths_;
+    }
+};
+
+struct Predicated final : Condition
+{
+    private:
+    bool predicated_;
+
+    public:
+    explicit Predicated(bool predicated) : predicated_(predicated) { }
+
+    Predicated() = default;
+    explicit Predicated(const Predicated&) = default;
+    Predicated(Predicated&&) = default;
+
+    private:
+    std::unique_ptr<Condition> clone() const override { return std::make_unique<Predicated>(predicated_); }
+
+    public:
+    bool predicated() const { return predicated_; }
+
+    bool implied_by(const Condition &o) const override {
+        auto other = cast<const Predicated>(&o);
+        if (not other) return false;
+        return other->predicated_ == this->predicated_;
+    }
+
+    void project_and_rename(const std::vector<std::pair<Schema::Identifier, Schema::Identifier>>&) override { }
+
+    bool operator==(const Condition &o) const override {
+        auto other = cast<const Predicated>(&o);
+        if (not other) return false;
+        return this->predicated_ == other->predicated_;
     }
 };
 
@@ -104,19 +276,53 @@ struct ConditionSet
 {
     private:
     ///> assigns a *unique* identifier to each type of `Condition`
-    std::unordered_map<std::type_index, std::unique_ptr<Condition>> type2cond;
+    std::unordered_map<std::type_index, std::unique_ptr<Condition>> type2cond_;
 
     public:
-    template<typename Condition>
-    std::enable_if_t<std::is_final_v<Condition>, void>
-    add_condition(Condition &&cond) {
-        auto p = std::make_unique<Condition>(std::move(cond)); // move-construct on heap
-        auto [it, res] = type2cond.try_emplace(typeid(Condition), std::move(p));
+    ConditionSet() = default;
+    explicit ConditionSet(const ConditionSet &other) {
+        for (auto &p : other.type2cond_)
+            this->type2cond_.emplace(p.first, p.second->clone());
+    }
+    ConditionSet(ConditionSet&&) = default;
+
+    ConditionSet & operator=(ConditionSet&&) = default;
+
+    template<typename Cond>
+    requires std::is_base_of_v<Condition, Cond> and std::is_final_v<Cond>
+    void add_condition(Cond &&cond) {
+        auto p = std::make_unique<Cond>(std::forward<Cond>(cond)); // move-construct on heap
+        auto [it, res] = type2cond_.try_emplace(typeid(Cond), std::move(p));
         if (not res)
-            throw std::logic_error("Condition of that type already exists in the ConditionSet");
+            throw invalid_argument("Condition of that type already exists in the ConditionSet");
     }
 
-    bool implied_by(const ConditionSet &other);
+    template<typename Cond>
+    requires std::is_base_of_v<Condition, Cond> and std::is_final_v<Cond>
+    void add_or_replace_condition(Cond &&cond) {
+        auto p = std::make_unique<Cond>(std::forward<Cond>(cond)); // move-construct on heap
+        type2cond_.insert_or_assign(typeid(Cond), std::move(p));
+    }
+
+    template<typename Cond>
+    requires std::is_base_of_v<Condition, Cond> and std::is_final_v<Cond>
+    Cond & get_condition() {
+        auto it = type2cond_.find(typeid(Cond));
+        M_insist(it != type2cond_.cend(), "condition not found");
+        return as<Cond>(*it->second);
+    }
+    template<typename Cond>
+    requires std::is_base_of_v<Condition, Cond> and std::is_final_v<Cond>
+    const Cond & get_condition() const { return const_cast<ConditionSet*>(this)->get_condition<Cond>(); }
+
+    bool empty() const { return type2cond_.empty(); }
+
+    bool implied_by(const ConditionSet &other) const;
+
+    void project_and_rename(const std::vector<std::pair<Schema::Identifier, Schema::Identifier>> &old2new);
+
+    bool operator==(const ConditionSet &other) const;
+    bool operator!=(const ConditionSet &other) const { return not operator==(other); }
 };
 
 
--- src/IR/Condition.cpp
+++ src/IR/Condition.cpp
@@ -4,14 +4,35 @@
 using namespace m;
 
 
-bool ConditionSet::implied_by(const ConditionSet &other)
+bool ConditionSet::implied_by(const ConditionSet &other) const
 {
-    for (auto &this_entry : this->type2cond) {
-        auto it = other.type2cond.find(this_entry.first);
-        if (it == other.type2cond.cend())
-            return false; // condition does not exist in other
+    for (auto &this_entry : this->type2cond_) {
+        auto it = other.type2cond_.find(this_entry.first);
+        if (it == other.type2cond_.cend())
+            return false; // condition not found
         if (not this_entry.second->implied_by(*it->second))
-            return false; // condition exists in other, but does not imply this condition
+            return false; // condition does not imply this condition
+    }
+    return true;
+}
+
+void ConditionSet::project_and_rename(const std::vector<std::pair<Schema::Identifier, Schema::Identifier>> &old2new)
+{
+    for (auto &entry : type2cond_)
+        entry.second->project_and_rename(old2new);
+}
+
+bool ConditionSet::operator==(const ConditionSet &other) const
+{
+    if (this->type2cond_.size() != other.type2cond_.size())
+        return false; // different number of conditions
+
+    for (auto &this_entry : this->type2cond_) {
+        auto it = other.type2cond_.find(this_entry.first);
+        if (it == other.type2cond_.cend())
+            return false; // condition not found
+        if (*this_entry.second != *it->second)
+            return false; // condition differs
     }
     return true;
 }

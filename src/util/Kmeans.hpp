3717c572ff77e332ccc253d6ca350313b4124edc
Immanuel Haffner <haffner.immanuel@gmail.com>
2023-02-15 09:15:33 +0100

    [API] Add missing `M_EXPORT`s.

--- src/util/Kmeans.hpp
+++ src/util/Kmeans.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Eigen/Dense>
+#include <mutable/mutable.hpp>
 #include <vector>
 
 
@@ -14,7 +15,7 @@ using MatrixRXf = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::Ro
  * @param k    the number of initial centroids to compute
  * @return     a `MatrixRXf` of `k` data points (rows) chosen as the `k` initial centroids for *k*-means
  */
-MatrixRXf kmeans_plus_plus(const Eigen::MatrixXf &data, unsigned k);
+MatrixRXf M_EXPORT kmeans_plus_plus(const Eigen::MatrixXf &data, unsigned k);
 
 /** Clusters the given data according to the *k*-means algorithm.
  *
@@ -27,7 +28,7 @@ MatrixRXf kmeans_plus_plus(const Eigen::MatrixXf &data, unsigned k);
  * @return     a `std::pair` of a `std::vector<unsigned>` assigning a label to each data point and an `Eigen::Matrix` of
  *             `k` rows with the centroids of the formed clusters
  */
-std::pair<std::vector<unsigned>, MatrixRXf> kmeans_with_centroids(const Eigen::MatrixXf &data, unsigned k);
+std::pair<std::vector<unsigned>, MatrixRXf> M_EXPORT kmeans_with_centroids(const Eigen::MatrixXf &data, unsigned k);
 
 /** Clusters the given data according to the *k*-means algorithm.
  *
@@ -39,7 +40,7 @@ std::pair<std::vector<unsigned>, MatrixRXf> kmeans_with_centroids(const Eigen::M
  * @param k    the number of clusters to form (more like an upper bound, as clusters may be empty)
  * @return     a `std::vector<unsigned>` assigning a label to each data point
  */
-inline std::vector<unsigned> kmeans(const Eigen::MatrixXf &data, unsigned k) {
+inline std::vector<unsigned> M_EXPORT kmeans(const Eigen::MatrixXf &data, unsigned k) {
     return kmeans_with_centroids(data, k).first;
 }
 

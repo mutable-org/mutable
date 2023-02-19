16d0832e903e1d4b9da7d641b140eafc5e8ac193
Immanuel Haffner <haffner.immanuel@gmail.com>
2023-02-19 01:41:40 +0100

    [API] Lay out class hierarchy of `DatabaseCommand`

--- include/mutable/catalog/DatabaseCommand.hpp
+++ include/mutable/catalog/DatabaseCommand.hpp
@@ -1,19 +1,33 @@
 #pragma once
 
+#include <mutable/parse/AST.hpp>
 #include <mutable/util/Diagnostic.hpp>
 #include <vector>
 
 
 namespace m {
 
+// forward declarations
+struct ConstDatabaseCommandVisitor;
+struct DatabaseCommandVisitor;
+
+
 /** The command pattern for operations in the DBMS. */
 struct DatabaseCommand
 {
     virtual ~DatabaseCommand() = default;
 
+    virtual void accept(DatabaseCommandVisitor &v) = 0;
+    virtual void accept(ConstDatabaseCommandVisitor &v) const = 0;
+
     /** Executes the command. */
     virtual void execute(Diagnostic &diag) const = 0;
 };
+
+
+/*======================================================================================================================
+ * Instructions
+ *====================================================================================================================*/
 
 /** A `DatabaseInstruction` represents an invokation of an *instruction*, i.e. an input starting with `\`, an
  * instruction name, and the arguments for that instruction. A new instruction can be implemented by making a new
@@ -36,7 +50,136 @@ struct learn_spns : DatabaseInstruction
 {
     learn_spns(std::vector<std::string> args) : DatabaseInstruction(std::move(args)) { }
 
+    void accept(DatabaseCommandVisitor &v) override;
+    void accept(ConstDatabaseCommandVisitor &v) const override;
+
     void execute(Diagnostic &diag) const override;
 };
+
+#define M_DATABASE_INSTRUCTION_LIST(X) \
+    X(learn_spns)
+
+
+/*======================================================================================================================
+ * Structured Query Language (SQL)
+ *====================================================================================================================*/
+
+struct SQLCommand : DatabaseCommand { };
+
+
+/*======================================================================================================================
+ * Data Manipulation Language (DML)
+ *====================================================================================================================*/
+
+/** Base class for all commands resulting from a *data manipulation language* (DML) statement. */
+struct DMLCommand : SQLCommand { };
+
+/** Run a query against the selected database. */
+struct QueryDatabase : DMLCommand
+{
+    private:
+    ast::SelectStmt stmt_;
+    // TODO more info? e.g. selected database?
+
+    public:
+    QueryDatabase(ast::SelectStmt stmt) : stmt_(std::move(stmt)) { }
+
+    void accept(DatabaseCommandVisitor &v) override;
+    void accept(ConstDatabaseCommandVisitor &v) const override;
+
+    void execute(Diagnostic &diag) const override;
+};
+
+/** Insert records into a `Table` of a `Database`. */
+struct InsertRecords : DMLCommand
+{
+    InsertRecords() { }
+
+    void accept(DatabaseCommandVisitor &v) override;
+    void accept(ConstDatabaseCommandVisitor &v) const override;
+
+    void execute(Diagnostic &diag) const override;
+};
+
+/** Modify records of a `Table` of a `Database`. */
+struct UpdateRecords : DMLCommand
+{
+    void accept(DatabaseCommandVisitor &v) override;
+    void accept(ConstDatabaseCommandVisitor &v) const override;
+
+    void execute(Diagnostic &diag) const override;
+};
+
+/** Delete records from a `Table` of a `Database`. */
+struct DeleteRecords : DMLCommand
+{
+    void accept(DatabaseCommandVisitor &v) override;
+    void accept(ConstDatabaseCommandVisitor &v) const override;
+
+    void execute(Diagnostic &diag) const override;
+};
+
+/** Import records from a *delimiter separated values* (DSV) file into a `Table` of a `Database`. */
+struct ImportDSV : DMLCommand
+{
+    void accept(DatabaseCommandVisitor &v) override;
+    void accept(ConstDatabaseCommandVisitor &v) const override;
+
+    void execute(Diagnostic &diag) const override;
+};
+
+#define M_DATABASE_DML_LIST(X) \
+    X(QueryDatabase) \
+    X(InsertRecords) \
+    X(UpdateRecords) \
+    X(DeleteRecords) \
+    X(ImportDSV)
+
+
+/*======================================================================================================================
+ * Data Definition Language
+ *====================================================================================================================*/
+
+struct DDLCommand : SQLCommand { };
+
+struct CreateDatabaseCommand : DDLCommand
+{
+    void accept(DatabaseCommandVisitor &v) override;
+    void accept(ConstDatabaseCommandVisitor &v) const override;
+
+    void execute(Diagnostic &diag) const override;
+};
+
+struct UseDatabaseCommand : DDLCommand
+{
+    void accept(DatabaseCommandVisitor &v) override;
+    void accept(ConstDatabaseCommandVisitor &v) const override;
+
+    void execute(Diagnostic &diag) const override;
+};
+
+struct CreateTableCommand : DDLCommand
+{
+    void accept(DatabaseCommandVisitor &v) override;
+    void accept(ConstDatabaseCommandVisitor &v) const override;
+
+    void execute(Diagnostic &diag) const override;
+};
+
+#define M_DATABASE_DDL_LIST(X)\
+    X(CreateDatabaseCommand) \
+    X(UseDatabaseCommand) \
+    X(CreateTableCommand)
+
+#define M_DATABASE_SQL_LIST(X) \
+    M_DATABASE_DML_LIST(X) \
+    M_DATABASE_DDL_LIST(X)
+
+#define M_DATABASE_COMMAND_LIST(X) \
+    M_DATABASE_INSTRUCTION_LIST(X) \
+    M_DATABASE_SQL_LIST(X)
+
+M_DECLARE_VISITOR(DatabaseCommandVisitor, DatabaseCommand, M_DATABASE_COMMAND_LIST)
+M_DECLARE_VISITOR(ConstDatabaseCommandVisitor, const DatabaseCommand, M_DATABASE_COMMAND_LIST)
 
 }
--- src/catalog/DatabaseCommand.cpp
+++ src/catalog/DatabaseCommand.cpp
@@ -7,6 +7,10 @@
 using namespace m;
 
 
+/*======================================================================================================================
+ * Instructions
+ *====================================================================================================================*/
+
 void learn_spns::execute(Diagnostic &diag) const
 {
     auto &C = Catalog::Get();
@@ -32,3 +36,59 @@ static void register_instructions()
     REGISTER(learn_spns, "create an SPN for every table in the database");
 #undef REGISTER
 }
+
+/*======================================================================================================================
+ * Data Manipulation Language (DML)
+ *====================================================================================================================*/
+
+void QueryDatabase::execute(Diagnostic&) const
+{
+    M_unreachable("not yet implemented");
+}
+
+void InsertRecords::execute(Diagnostic&) const
+{
+    M_unreachable("not yet implemented");
+}
+
+void UpdateRecords::execute(Diagnostic&) const
+{
+    M_unreachable("not yet implemented");
+}
+
+void DeleteRecords::execute(Diagnostic&) const
+{
+    M_unreachable("not yet implemented");
+}
+
+void ImportDSV::execute(Diagnostic&) const
+{
+    M_unreachable("not yet implemented");
+}
+
+
+/*======================================================================================================================
+ * Data Definition Language
+ *====================================================================================================================*/
+
+void CreateDatabaseCommand::execute(Diagnostic&) const
+{
+    M_unreachable("not yet implemented");
+}
+
+void UseDatabaseCommand::execute(Diagnostic&) const
+{
+    M_unreachable("not yet implemented");
+}
+
+void CreateTableCommand::execute(Diagnostic&) const
+{
+    M_unreachable("not yet implemented");
+}
+
+
+#define ACCEPT(CLASS) \
+    void CLASS::accept(DatabaseCommandVisitor &v) { v(*this); } \
+    void CLASS::accept(ConstDatabaseCommandVisitor &v) const { v(*this); }
+M_DATABASE_COMMAND_LIST(ACCEPT)
+#undef ACCEPT

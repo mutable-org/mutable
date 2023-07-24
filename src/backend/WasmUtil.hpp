0ac9439cfe1ceb3c4c1d8ea54f1b74b6fb30cfe9
Immanuel Haffner <haffner.immanuel@gmail.com>
2023-07-24 10:30:32 +0200

    [Wasm] Fix constraint.

--- src/backend/WasmUtil.hpp
+++ src/backend/WasmUtil.hpp
@@ -532,7 +532,7 @@ struct Environment
     }
      ///> Compile \p t by delegating compilation to an `ExprCompiler` for `this` `Environment`.
     template<typename T, typename U>
-    requires requires (ExprCompiler C, U &&u) { C.compile(std::forward<U>(u)); }
+    requires requires (ExprCompiler C, U &&u) { C.compile<T>(std::forward<U>(u)); }
     auto compile(U &&u) const {
         ExprCompiler C(*this);
         return C.compile<T>(std::forward<U>(u));

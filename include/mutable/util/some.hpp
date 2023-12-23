60dc1904909e69490d92d49438aecccee7e8875f
Immanuel Haffner <haffner.immanuel@gmail.com>
2023-12-23 03:21:31 +0100

    [Util] Add doc & constraints to helper type `some`

--- include/mutable/util/some.hpp
+++ include/mutable/util/some.hpp
@@ -7,17 +7,34 @@
 
 namespace m {
 
+/**
+ * A helper type to wrap some template type parameter such that it can safely be used to declare a class field or local
+ * variable.  In particular, handles `void`.
+ */
 template<typename T>
 struct some
 {
     using value_type = std::remove_reference_t<T>;
     value_type value;
-    explicit some(value_type &&value) : value(std::move(value)) { }
+
+    explicit some(value_type &&value)
+        requires requires (value_type &&value) { { value_type(std::move(value)) } -> std::same_as<value_type>; }
+    : value(std::move(value)) { }
+
     some() = delete;
     some(const some&) = delete;
-    some(some &&other) : value(std::move(other.value)) { }
-    some & operator=(some other) { this->value = std::move(other.value); return *this; }
-    some & operator=(value_type &&value) { this->value = std::move(value); return *this; }
+
+    some(some &&other)
+        requires requires (some &&other) { { value_type(std::move(other.value)) } -> std::same_as<value_type>; }
+    : value(std::move(other.value)) { }
+
+    some & operator=(some other)
+        requires requires (value_type &this_value, some other) { this_value = std::move(other.value); }
+    { this->value = std::move(other.value); return *this; }
+
+    some & operator=(value_type &&value)
+        requires requires (value_type &this_value, value_type &&other_value) { this_value = std::move(other_value); }
+    { this->value = std::move(value); return *this; }
 };
 
 template<>

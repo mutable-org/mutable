241684c87c6ce79a5145903a50e4c25f3ecc6dc1
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-01-05 15:32:59 +0100

    [Util] Add and document mu*t*able exceptions.

--- include/mutable/util/exception.hpp
+++ include/mutable/util/exception.hpp
@@ -17,16 +17,34 @@ struct exception : std::exception
     const char * what() const noexcept override { return message_.c_str(); }
 };
 
-struct invalid_argument : exception
+/**
+ * Base class for exceptions signaling an error in the logic.  Try to use a more precise subclass of `logic_error`
+ * whenever you can.  Feel free to extend the subclasses as you deem useful.
+ */
+struct logic_error : exception
 {
-    explicit invalid_argument(std::string message) : exception(std::move(message)) { }
+    explicit logic_error(std::string message) : exception(std::move(message)) { }
 };
 
-struct out_of_range : exception
+/** Signals that an object was in an invalid state when a method was invoked. */
+struct invalid_state : logic_error
 {
-    explicit out_of_range(std::string message) : exception(std::move(message)) { }
+    explicit invalid_state(std::string message) : logic_error(std::move(message)) { }
 };
 
+/** Signals that an argument to a function of method was invalid. */
+struct invalid_argument : logic_error
+{
+    explicit invalid_argument(std::string message) : logic_error(std::move(message)) { }
+};
+
+/** Signals that an index-based or key-based access was out of range. */
+struct out_of_range : logic_error
+{
+    explicit out_of_range(std::string message) : logic_error(std::move(message)) { }
+};
+
+/** Signals a runtime error that mu*t*able is not responsible for and that mu*t*able was not able to recover from. */
 struct runtime_error : exception
 {
     explicit runtime_error(std::string message) : exception(std::move(message)) { }

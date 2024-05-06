73de0d334a037076fe8bac8e32e5733e6e22ae1a
Immanuel Haffner <haffner.immanuel@gmail.com>
2024-05-06 14:36:48 +0200

    [CMake] Fix depot_tools.cmake
    Fix incorrect use of `CMAKE_CURRENT_SOURCE_DIR`, which is relative to
    the current `CMakeLists.txt` being processed, and correctly use
    `CMAKE_SOURCE_DIR` instead, which always refers to the project root.

--- CMakeFiles/depot_tools.cmake
+++ CMakeFiles/depot_tools.cmake
@@ -2,13 +2,14 @@
 FetchContent_Populate(
     Depot_tools
     PREFIX third-party
-    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third-party"
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/third-party"
     GIT_REPOSITORY "https://chromium.googlesource.com/chromium/tools/depot_tools.git"
     GIT_TAG 530d86d40b2aab70e0541ea0f296388ec09f0576
-    SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third-party/depot_tools"
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/third-party/depot_tools"
     SYSTEM
     EXCLUDE_FROM_ALL
 )
-find_program(DEPOT_TOOLS_FETCH fetch PATHS "${CMAKE_CURRENT_SOURCE_DIR}/third-party/depot_tools" REQUIRED)
-find_program(DEPOT_TOOLS_GCLIENT gclient PATHS "${CMAKE_CURRENT_SOURCE_DIR}/third-party/depot_tools" REQUIRED)
-find_program(DEPOT_TOOLS_GN gn PATHS "${CMAKE_CURRENT_SOURCE_DIR}/third-party/depot_tools" REQUIRED)
+set(DEPOT_TOOLS_PATH "${CMAKE_SOURCE_DIR}/third-party/depot_tools")
+find_program(DEPOT_TOOLS_FETCH fetch PATHS "${CMAKE_SOURCE_DIR}/third-party/depot_tools" REQUIRED)
+find_program(DEPOT_TOOLS_GCLIENT gclient PATHS "${CMAKE_SOURCE_DIR}/third-party/depot_tools" REQUIRED)
+find_program(DEPOT_TOOLS_GN gn PATHS "${CMAKE_SOURCE_DIR}/third-party/depot_tools" REQUIRED)

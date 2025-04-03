24c36cb1d8f641326a9b0c53996b47a338822732
Marcel Maltry <mmaltry+git@mailbox.org>
2025-04-03 11:22:53 +0200

    [CMake] Update Replxx to `release-0.0.4`

--- CMakeFiles/Replxx.cmake
+++ CMakeFiles/Replxx.cmake
@@ -9,10 +9,13 @@ ExternalProject_Add(
     PREFIX third-party
     DOWNLOAD_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third-party"
     GIT_REPOSITORY "https://github.com/AmokHuginnsson/replxx.git"
-    GIT_TAG 737c8e8147d44eae530e2b56592ad8788695ef12
+    GIT_TAG 2b248467112cbbc16f63fde38230fd7c7ffc55f1
     SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/third-party/replxx"
-    PATCH_COMMAND sed -i.origin "s/^.*set_property.*DEBUG_POSTFIX.*//" "CMakeLists.txt"
-    COMMAND       sed -i.origin "s/^if *( *NOT BUILD_SHARED_LIBS AND MSVC *)/if ( NOT BUILD_SHARED_LIBS )/" "CMakeLists.txt"
+    PATCH_COMMAND
+        sed -i.origin "s/^.*set_property.*DEBUG_POSTFIX.*//" "CMakeLists.txt" &&
+        sed -i.origin "s/^.*set_property.*RELWITHDEBINFO_POSTFIX.*//" "CMakeLists.txt" &&
+        sed -i.origin "s/^.*set_property.*MINSIZEREL_POSTFIX.*//" "CMakeLists.txt" &&
+        sed -i.origin "s/^if *( *NOT BUILD_SHARED_LIBS AND MSVC *)/if ( NOT BUILD_SHARED_LIBS )/" "CMakeLists.txt"
     CMAKE_ARGS -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS} -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
     CONFIGURE_HANDLED_BY_BUILD true
     INSTALL_COMMAND ""

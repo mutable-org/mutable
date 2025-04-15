1a27c2be97c2d9c6125d1ded635587baff1e4f41
Marcel Maltry <mmaltry+git@mailbox.org>
2025-04-15 16:06:03 +0200

    [CMake] Update Json to `v3.11.2`

--- CMakeFiles/nlohmann_json.cmake
+++ CMakeFiles/nlohmann_json.cmake
@@ -1,5 +1,5 @@
 # nlohmann_json - A Simple, Header-Only JSON Parser
-set(JSON_VERSION "v3.9.0") # dcfd3ee9de4e50e077c230cbf27c77aa8d760327
+set(JSON_VERSION "v3.11.2") # 67e6070f9d9a44b4dec79ebe6b591f39d2285593
 FetchContent_Populate(
     Json
     GIT_REPOSITORY https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent.git
@@ -8,4 +8,4 @@ FetchContent_Populate(
     SYSTEM
     EXCLUDE_FROM_ALL
 )
-include_directories(SYSTEM "${CMAKE_CURRENT_SOURCE_DIR}/third-party/json-${JSON_VERSION}/include")
+include_directories(SYSTEM "${CMAKE_CURRENT_SOURCE_DIR}/third-party/json-${JSON_VERSION}/single_include")

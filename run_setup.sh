a828cd0a6c5a92966417d4eddf2ca52409dab2e7
Oliver Czerwiak <oliverczerwiak@outlook.com>
2025-05-17 12:29:50 +0200

    log build process

--- run_setup.sh
+++ run_setup.sh
@@ -4,6 +4,6 @@
 chmod +x configure_command.sh
 chmod +x build_command.sh
 
-# Run the scripts in order
-./configure_command.sh
-./build_command.sh
+# Run the scripts in order and log their outputs
+./configure_command.sh > configure_command.log 2>&1
+./build_command.sh > build_command.log 2>&1

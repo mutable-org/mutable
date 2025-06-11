adcf6b79d7881e37244efac7b959652d8281f23e
Oliver Czerwiak <oliverczerwiak@outlook.com>
2025-06-11 08:57:17 +0200

    add 2 and 3 way join

--- small_queries.sql
+++ small_queries.sql
@@ -23,3 +23,8 @@ CREATE TABLE table_4 (
 IMPORT INTO table_4 DSV "benchmark/small/data/table4.csv";
 -- same but in a single line
 SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2, table_3.id, table_3.col_3, table_4.id, table_4.col_4 FROM table_1, table_2, table_3, table_4 WHERE table_1.id = table_2.id AND table_1.id = table_3.id AND table_1.id = table_4.id AND table_1.col_1 < 500 LIMIT 10;
+
+
+SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2, table_3.id, table_3.col_3 FROM table_1, table_2, table_3 WHERE table_1.id = table_2.id AND table_1.id = table_3.id;
+
+SELECT table_1.id, table_1.col_1, table_2.id, table_2.col_2 FROM table_1, table_2 WHERE table_1.id = table_2.id;
